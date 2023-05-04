2779b730062431d8ecc5e1278f88da29ff70fe88
mahasachan <mahasachan.sa@ku.th>
2023-05-04 20:42:47 +0700

    init layout

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,47 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
-<template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+<template >
+  <!-- Navbar -->
+  <div class="flex flex-[1] w-screen h-screen">
+    <aside class="flex">
+      <div class="w-72 bg-green-300"></div>
+    </aside>
+    <!--End Navbar-->
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <main class="flex flex-col grow pb-[12px]">
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
+      <!-- header space-->
+      <header class="h-[8%]"></header>
+      <!--End header space-->
 
-  <RouterView />
+      <section class="flex h-full">
+
+        <!--container chat left-->
+        <div class="flex-[1] bg-green-100 rounded-[18px] mx-[10px]"></div>
+        <!--End container chat left-->
+
+        <!-- container chat center -->
+        <div class="flex-[2] bg-green-100 rounded-[18px] mx-[10px] min-w-[512px]"></div>
+        <!--End container chat center -->
+
+        <!--container chat right-->
+        <div class="flex-[1] bg-green-100 rounded-[18px] mx-[10px]"></div>
+        <!--End container chat right-->
+
+      </section>
+
+      <!-- footer space-->
+      <footer class="h-[3%]"></footer>
+      <!--End footer space-->
+
+    </main>
+  </div>
+  
+
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+<style>
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
