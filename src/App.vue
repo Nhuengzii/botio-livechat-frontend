11efb774e77765aacc436779204b88ce8167a302
Nhuengzii <32nhueng@gmail.com>
2023-05-07 11:48:48 +0700

    create router logic and layout

--- src/App.vue
+++ src/App.vue
@@ -11,37 +11,9 @@ import { RouterLink, RouterView } from 'vue-router'
     <!--End Navbar-->
 
     <main class="flex flex-col grow pb-[12px]">
-
-      <!-- header space-->
-      <header class="h-[8%]"></header>
-      <!--End header space-->
-
-      <section class="flex h-full">
-
-        <!--container chat left-->
-        <div class="flex-[1] bg-green-100 rounded-[18px] mx-[10px]"></div>
-        <!--End container chat left-->
-
-        <!-- container chat center -->
-        <div class="flex-[2] bg-green-100 rounded-[18px] mx-[10px] min-w-[512px]"></div>
-        <!--End container chat center -->
-
-        <!--container chat right-->
-        <div class="flex-[1] bg-green-100 rounded-[18px] mx-[10px]"></div>
-        <!--End container chat right-->
-
-      </section>
-
-      <!-- footer space-->
-      <footer class="h-[3%]"></footer>
-      <!--End footer space-->
-
+      <RouterView />
     </main>
   </div>
-  
-
 </template>
 
-<style>
-
-</style>
+<style></style>
