81e454414fe233fb2fd45173aae0d20d482e6041
Nhuengzii <32nhueng@gmail.com>
2023-05-07 13:32:36 +0700

    create basic conversations store with some mock data

---
+++ src/stores/conversation.ts
@@ -0,0 +1,56 @@
+import { ref, computed } from "vue";
+import { defineStore } from "pinia";
+
+type Participant = {
+  userId: string,
+  username: string,
+  profilePicture: string,
+}
+
+type Conversation = {
+  shopId: string,
+  pageId: string,
+  conversationId: string,
+  conversationPicture: string,
+  updatedAt: number,
+  lastActivity: string,
+  participants: Participant[],
+}
+
+export const useConversationStore = defineStore("conversation", () => {
+  const mockConversations: Conversation[] = [
+    {
+      shopId: "1",
+      pageId: "1",
+      conversationId: "1",
+      conversationPicture: "https://picsum.photos/200",
+      updatedAt: 234789,
+      lastActivity: "Boom send nude picutes",
+      participants: [
+        {
+          userId: "1",
+          username: "Boom",
+          profilePicture: "https://avatars.githubusercontent.com/u/124413969?v=4"
+        }
+      ]
+    },
+    {
+      shopId: "1",
+      pageId: "1",
+      conversationId: "2",
+      conversationPicture: "https://picsum.photos/200",
+      updatedAt: 2347892,
+      lastActivity: "Hello สวัสดี",
+      participants: [
+        {
+          userId: "1",
+          username: "Nhuengzii",
+          profilePicture: "https://avatars.githubusercontent.com/u/86766644?v=4",
+        }
+      ]
+    }
+  ]
+  const conversations = ref<Conversation[]>(mockConversations);
+  return { conversations }
+});
+
