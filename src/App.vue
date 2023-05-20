b963b8ea14cdea979bd844095bc3bbc4c656757c
Nhuengzii <32nhueng@gmail.com>
2023-05-20 23:47:25 +0700

    use websocket store instead of directly use websocket

--- src/App.vue
+++ src/App.vue
@@ -2,58 +2,17 @@
 import { onMounted, onUnmounted } from 'vue';
 import { RouterLink, RouterView } from 'vue-router'
 import Navbar from './components/Navbar.vue';
-import { useConversationStore } from "./stores/conversation";
-const conversationStore = useConversationStore();
-type Source = {
-  sourceID: string,
-  sourceName?: string
-  sourcePicture?: string,
-  sourceType: "USER" | "ADMIN",
-}
-
-type Message = {
-  conversationID: string,
-  messageID: string,
-  timeStamp: number,
-  source: Source,
-  message: string,
-}
+import { useWebsocketStore } from './stores/websocket';
+const websocketStore = useWebsocketStore();
 onMounted(() => {
   console.log('App mounted!');
-  const socket = new WebSocket('wss://mgec4plwk7.execute-api.ap-southeast-1.amazonaws.com/test?shopId=1');
-  socket.onopen = () => {
-    console.log('WebSocket connected successfully');
-  };
-  socket.onclose = () => {
-    console.log('WebSocket disconnected');
-  };
-  socket.onerror = (err) => {
-    console.log('WebSocket error', err);
-  };
-  socket.onmessage = (event) => {
-    const data = JSON.parse(event.data);
-    console.log(JSON.stringify(data, null, 2))
-
-    const newMessage: Message = {
-      conversationID: data.conversationID,
-      messageID: data.messageID,
-      timeStamp: data.timestamp,
-      source: {
-        sourceID: data.source.userID,
-        sourceType: data.source.userType === "user" ? "USER" : "ADMIN",
-      },
-      message: data.message,
-    }
-    conversationStore.addMessage(newMessage.conversationID, newMessage);
-
-
-  }
-  onUnmounted(() => {
-    console.log('App unmounted!');
-    socket.close();
-  });
+  websocketStore.connect();
 });
 
+onUnmounted(() => {
+  console.log('App unmounted!');
+  websocketStore.disconnect();
+});
 </script>
 
 <template >
