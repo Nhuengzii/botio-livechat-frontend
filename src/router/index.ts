7b79a88548efbb091e697699fcebbb5c8692d774
Nhuengzii <32nhueng@gmail.com>
2023-05-21 00:18:25 +0700

    make root path and redirect to facebook section

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,6 +6,11 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      name: 'root',
+      path: '/',
+      redirect: '/fb/'
+    },
+    {
       path: '/:platform',
       name: 'LivechatView',
       component: LivechatView,
