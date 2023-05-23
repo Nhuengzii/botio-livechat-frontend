b34c3727a65ce47fd52af127d174195f8e950100
peebaku <baramee.254450@gmail.com>
2023-05-23 15:35:54 +0700

    position

--- src/components/HeaderChatLeft.vue
+++ src/components/HeaderChatLeft.vue
@@ -1,6 +1,6 @@
 <template>
-    <aside class="flex-row justify-center w-[25%] h-[100%] bg-green-400 rounded-[18px] p-1 duration-500 border-solid border-glass relative  mt-2  shadow-inner">
-        <div v-if="$route.params.platform === 'facebook'" class="w-full h-full">
+    <aside class="flex-row justify-center w-[25%] h-[25%] bg-green-400 rounded-t-[18px] mb-1 p-1 duration-500 border-solid border-glass relative    shadow-inner">
+        <div v-if="$route.params.platform === 'facebook'" class="w-full h-full overflow-clip">
             <div class="grid grid-cols-1 px-[14px] pb-[1.25rem] pt-[20px] w-full">
                 <div class="flex gap-3 items-center   rounded-2xl ml-3">
                     <div>ข้อความทั้งหมด
@@ -12,7 +12,7 @@
                 </div>
             </div>
             <!-- end num message box-->
-            <div class=" flex bg-blue-100 justify-around py-[1rem]">
+            <div class=" flex    justify-around py-[1rem] ">
                 <button class="flex bg-blue-200 w-[85%] h-full items-center justify-start py-[.75rem] rounded-[20px]">
                     <div class="pl-[20px]">
                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
@@ -28,43 +28,7 @@
             </div>
             <!-- end search name box-->
 
-            <!--- conversation list-->
-            <div class="relative flex flex-col overflow-hidden w-full h-full ">
-                <div class="relative w-full h-full bg-white overflow-hidden">
-                    <div class="max-h-[80%] absolute inset-0 top-0 left-0 right-0 overflow-scroll scrollbar-hide pb-[36px]">
-                        <div class="relative flex flex-col">
-                            <div v-for="{ conversationID, conversationPicture, lastActivity, participants } in conversationsStore.conversations"
-                                class="flex py-[4px] pr-[4px]">
-                                <div class="w-full ">
-                                    <router-link :to="{ name: 'Conver', params: { conversation_id: conversationID } }"
-                                        class="flex px-[14px] pt-[20px] pb-[1.25rem]  border-2 rounded-[20px]">
-                                        <div class="grow-1 shrink-0 w-[45px] h-[45px] mr-[20px]">
-                                            <div class="overflow-hidden w-[45px] h-[45px] bg-blue-200 rounded-full">
-                                                <img v-if="conversationPicture" :src="conversationPicture" alt=""
-                                                    class="object-cover w-full h-full rounded-full">
-                                                <img v-else :src="participants[0].profilePicture" alt=""
-                                                    class="object-cover w-full h-full rounded-full">
-                                            </div>
-                                        </div>
-                                        <div class="grow-2 overflow-hidden">
-                                            <div class="flex justify-between">
-                                                <span class="text-sm font-semibold leading-6 text-gray-900">{{
-                                                    participants[0].username }}</span>
-                                            </div>
-                                            <div class="flex items-end justify-between whitespace-nowrap">
-                                                <span
-                                                    class="overflow-hidden text-ellipsis whitespace-nowrap mt-1 truncate text-xs leading-5 text-gray-500">{{
-                                                        lastActivity }}</span>
-                                            </div>
-                                        </div>
-                                    </router-link>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!--- end conversation list-->
+
 
 
         </div>
@@ -81,14 +45,5 @@ const conversationsStore = useConversationsStore();
 
 
 <style scoped>
-.scrollbar-hide::-webkit-scrollbar {
-    display: none;
-}
 
-.scrollbar-hide {
-    -ms-overflow-style: none;
-    /* IE and Edge */
-    scrollbar-width: none;
-    /* Firefox */
-}
 </style>
--- src/components/ShowListConversation.vue
+++ src/components/ShowListConversation.vue
@@ -1,11 +1,38 @@
 <template>
+    <!--- conversation list-->
 
+    <div v-for="{ conversationID, conversationPicture, lastActivity, participants } in conversationsStore.conversations"
+        class="h-[80px] flex-col px-4 justify-center bg-green-400 mb-1 border-neutral-600 w-[25%] overflow-clip">
+            <router-link :to="{ name: 'Conver', params: { conversation_id: conversationID } }"
+                class="flex px-[14px] pt-[20px] pb-[1.25rem] ">
+                <div class="grow-1 shrink-0 w-[45px] h-[45px] mr-[20px]">
+                    <div class="overflow-hidden w-[45px] h-[45px] bg-blue-200 ">
+                        <img v-if="conversationPicture" :src="conversationPicture" alt=""
+                            class="object-cover w-full h-full rounded-full">
+                        <img v-else :src="participants[0].profilePicture" alt=""
+                            class="object-cover w-full h-full rounded-full">
+                    </div>
+                </div>
+                <div class="grow-2 overflow-hidden">
+                    <div class="flex justify-between">
+                        <span class="text-sm font-semibold leading-6 text-gray-900">{{
+                            participants[0].username }}</span>
+                    </div>
+                    <div class="flex items-end justify-between whitespace-nowrap">
+                        <span
+                            class="text-ellipsis whitespace-nowrap mt-1 truncate text-xs leading-5 text-gray-500">{{
+                                lastActivity }}</span>
+                    </div>
+                </div>
+            </router-link>
+        </div>
+
+    <!--- end conversation list-->
 </template>
 
 <script setup lang="ts">
 import { useConversationsStore } from '@/stores/conversations';
 import { onBeforeMount } from 'vue';
-
 import { useRoute } from 'vue-router';
 const conversationsStore = useConversationsStore();
 const route = useRoute();
@@ -15,5 +42,14 @@ onBeforeMount(async () => {
 </script>
 
 <style scoped>
+.scrollbar-hide::-webkit-scrollbar {
+    display: none;
+}
 
+.scrollbar-hide {
+    -ms-overflow-style: none;
+    /* IE and Edge */
+    scrollbar-width: none;
+    /* Firefox */
+}
 </style>
