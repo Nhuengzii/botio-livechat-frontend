8b8d78421b35f9246d89b16e36a541b3a92b7957
Nhuengzii <32nhueng@gmail.com>
2023-06-12 02:08:14 +0700

    implements IBotioLivechat without actually implement

---
+++ src/lib/BotioLivechat.ts
@@ -0,0 +1,27 @@
+import type IBotioLivechat from "@/types/BotioLivechat";
+import type { Conversation } from "@/types/conversation";
+import type { Message } from "@/types/message";
+
+class BotioLivechat implements IBotioLivechat {
+  botioRestApiUrl: string;
+  botioWebsocketApiUrl: string;
+  constructor(botioRestApiUrl: string, botioWebsocketApiUrl: string) {
+    this.botioRestApiUrl = botioRestApiUrl;
+    this.botioWebsocketApiUrl = botioWebsocketApiUrl;
+  }
+  fetchConversations: (platform: string) => Promise<Conversation[]> = async (platform: string) => {
+    return []
+  };
+  fetchMessages: (platform: string, conversationId: string) => Promise<Message[]> = async (platform: string, conversationId: string) => {
+    return []
+  };
+  getConversation: (platform: string, conversationId: string) => Promise<Conversation | null> = async (platform: string, conversationId: string) => {
+    return null
+  };
+  getMessage: (platform: string, conversationId: string, messageId: string) => Promise<Message | null> = async (platform: string, conversationId: string, messageId: string) => {
+    return null
+  };
+  sendTextMessage: (platform: string, conversationId: string, text: string) => Promise<Message | null> = async (platform: string, conversationId: string, text: string) => {
+    return null
+  };
+}
