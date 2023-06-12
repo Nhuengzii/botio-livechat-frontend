c4bcd9c9523f4ad3ed4bae20e583d3a4656a1c27
Nhuengzii <32nhueng@gmail.com>
2023-06-12 22:18:02 +0700

    add conversation prop in Thread component

--- src/components/Thread.vue
+++ src/components/Thread.vue
@@ -1,10 +1,18 @@
 <template>
   <div>
     <h1>THis is Thread block</h1>
+    <h1>This me {{ conversation }}</h1>
   </div>
 </template>
 
 <script setup lang="ts">
+import type { Conversation } from '@/types/conversation';
+import type { PropType } from 'vue';
+defineProps({
+  conversation: {
+    type: Object as PropType<Conversation>,
+  }
+})
 
 </script>
 
