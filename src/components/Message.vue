3db8cc515af7f3a0624d9f5f2caafc224b4550c5
Nhuengzii <32nhueng@gmail.com>
2023-06-12 22:09:37 +0700

    define prop in Message components

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -1,11 +1,25 @@
 <template>
   <div>
     <h1>This is Message block</h1>
+    <h1>This is me {{ message }}</h1>
+    <h1>From {{ conversation }}</h1>
   </div>
 </template>
 
 <script setup lang="ts">
-
+import type { Message } from '@/types/message';
+import type { Conversation } from '@/types/conversation';
+import type { PropType } from 'vue';
+defineProps({
+  message: {
+    type: Object as PropType<Message>,
+    required: true
+  },
+  conversation: {
+    type: Object as PropType<Conversation>,
+    required: true
+  }
+})
 </script>
 
 <style scoped></style>
