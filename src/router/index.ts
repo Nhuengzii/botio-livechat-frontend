4815b23f2fdc368ea762b7e3e1ba45e280f1048a
Nhuengzii <32nhueng@gmail.com>
2023-06-12 15:05:32 +0700

    create livechat route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory, } from 'vue-router'
 import Livechat from '../views/Livechat.vue'
+import Empty from '../views/Empty.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,6 +8,11 @@ const router = createRouter({
     {
       path: '/',
       name: 'root',
+      component: () => Empty
+    },
+    {
+      path: '/livechat',
+      name: 'livechat',
       component: () => Livechat
     }
   ]
