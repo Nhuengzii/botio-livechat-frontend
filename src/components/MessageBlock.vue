1609629fae7a0ee67d670bdf39fe6ff8c604433e
Nhuengzii <32nhueng@gmail.com>
2023-06-13 06:02:43 +0700

    make MessageBlock use message content component

--- src/components/MessageBlock.vue
+++ src/components/MessageBlock.vue
@@ -1,20 +1,45 @@
 <template>
   <div class="bg-sky-50 m-3">
     <h1>This is Message block</h1>
-    <h1>This is me {{ message }}</h1>
-    <h1>From {{ conversation }}</h1>
+    <template v-if="messageType == 'NormalText'">
+      <div>
+        <NormalText :message="message" :conversation="conversation" />
+      </div>
+    </template>
+    <template v-else-if="messageType == 'ImageMessage'">
+      <div>
+        <ImageMessage :message="message" :conversation="conversation" />
+      </div>
+    </template>
+    <template v-else>
+      <div>
+        <UnsupportMessage :message="message" :conversation="conversation" />
+      </div>
+    </template>
   </div>
 </template>
 
 <script setup lang="ts">
 import type { Message } from '@/types/message';
 import type { Conversation } from '@/types/conversation';
+import NormalText from './MessageContents/NormalText.vue';
+import ImageMessage from './MessageContents/ImageMessage.vue';
+import UnsupportMessage from './MessageContents/UnsupportMessage.vue';
+import { ref } from 'vue';
 const { message, conversation } = defineProps<
   {
     message: Message
     conversation: Conversation
   }
 >()
+const messageType = ref('')
+if (message.message.length > 0) {
+  messageType.value = 'NormalText'
+} else if (message.attachments![0]?.attachmentType) {
+  messageType.value = 'ImageMessage'
+} else {
+  messageType.value = 'Unsupport'
+}
 </script>
 
 <style scoped></style>
