538a75f34eb29d4e16c1bc7e916d603d598f11c4
Nhuengzii <32nhueng@gmail.com>
2023-06-12 23:24:54 +0700

    implement example usage of vue3-tabs-chrome

--- src/views/subs/Chats.vue
+++ src/views/subs/Chats.vue
@@ -1,11 +1,24 @@
 <template>
   <div class="flex-[2] bg-green-200">
-    <h1> This Is Messages subviews </h1>
+    <vue3-tabs-chrome :tabs="tabs" v-model="tab" />
   </div>
 </template>
 
 <script setup lang="ts">
-
+import { ref } from "vue";
+import Vue3TabsChrome from "vue3-tabs-chrome"
+import 'vue3-tabs-chrome/dist/vue3-tabs-chrome.css'
+const tab = ref('google')
+const tabs = ref([
+  {
+    label: 'google',
+    key: 'google'
+  },
+  {
+    label: 'facebook',
+    key: 'facebook'
+  }
+])
 </script>
 
 <style scoped></style>
