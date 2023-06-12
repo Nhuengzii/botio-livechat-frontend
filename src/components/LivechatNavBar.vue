6d465be62b77728386acf0906c761a1b0d09fda5
peebaKU <baramee.254450@gmail.com>
2023-06-12 23:53:16 +0700

    add buttom icon application

--- src/components/LivechatNavBar.vue
+++ src/components/LivechatNavBar.vue
@@ -1,10 +1,30 @@
 <template>
   <div>
-    <h1>This is Livechat NavBar bar</h1>
+  <div :class="[uiStore.is_expanded? 'bg-[#EAEAEA] duration-300':'bg-[#394867] duration-300']" class="h-full" > 
+    <div class="p-4  " v-for=" platform in platforms">
+    <PlatformIcon platform=platform.platform :isFocus=platform.isfocus :unread=platform.unread    />
+    </div>
   </div>
+   </div>
 </template>
 
 <script setup lang="ts">
+  import { useUIStore } from '@/stores/UI';
+  import PlatformIcon from './PlatformIcon.vue';
+  const platforms = [{
+  platform: "instagram",
+  isfocus: true,
+  unread: 33
+},{
+  platform: "facebook",
+  isfocus: false,
+  unread: 10
+},{
+  platform: "line",
+  isfocus: false,
+  unread: 20
+}]
+  const uiStore = useUIStore();
 
 </script>
 
