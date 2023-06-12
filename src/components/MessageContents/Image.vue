0b372dd65e774a7504c9306cba2865be63972b99
Nhuengzii <32nhueng@gmail.com>
2023-06-13 05:48:31 +0700

    create Image message Component

---
+++ src/components/MessageContents/Image.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>This is Image Message</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { Message } from "@/types/message";
+import type { Conversation } from "@/types/conversation";
+defineProps<{
+  message: Message
+  conversation: Conversation
+}>()
+</script>
+
+<style scoped></style>
