a937031e8b6ecb0ca4562ee4556c223db84cf70a
Nhuengzii <32nhueng@gmail.com>
2023-06-12 15:31:43 +0700

    define besic Livechat view structure

--- src/views/Livechat.vue
+++ src/views/Livechat.vue
@@ -1,10 +1,18 @@
 <template>
   <div>
-
+    <h1>THIS is LiveChat view</h1>
+  </div>
+  <div>
+    <ConversationsThread />
+  </div>
+  <div>
+    <Chats />
   </div>
 </template>
 
 <script setup lang="ts">
+import ConversationsThread from './subs/ConversationsThread.vue';
+import Chats from './subs/Chats.vue';
 
 </script>
 
