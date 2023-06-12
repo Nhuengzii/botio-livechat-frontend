7b49a5b6980cad88e8e29795e20cd134e84d33ad
Nhuengzii <32nhueng@gmail.com>
2023-06-12 14:49:53 +0700

    clear script in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,4 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import Navbar from './components/Navbar.vue';
-import { useRoute } from 'vue-router';
-const route = useRoute();
-const platform = route.params.platform as string
 </script>
 
 <template ></template>
