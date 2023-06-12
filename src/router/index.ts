900bb1fd86c8f84252026f6ec14c918135087535
Nhuengzii <32nhueng@gmail.com>
2023-06-12 19:18:54 +0700

    define more route

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,13 +8,38 @@ const router = createRouter({
     {
       path: '/',
       name: 'root',
+      redirect: '/Start'
+    },
+    {
+      path: '/Start',
+      name: 'Start',
+      component: () => Empty
+    },
+    {
+      path: '/Services',
+      name: 'Services',
+      component: () => Empty
+    },
+    {
+      path: '/Stock',
+      name: 'Stock',
+      component: () => Empty
+    },
+    {
+      path: '/Order',
+      name: 'Order',
+      component: () => Empty
+    },
+    {
+      path: '/Modify',
+      name: 'Modify',
       component: () => Empty
     },
     {
       path: '/livechat',
       name: 'livechat',
       component: () => Livechat
-    }
+    },
   ]
 })
 
