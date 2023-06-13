13cb459699189ee1b0735beaa6d483e3dbe19bb9
Nhuengzii <32nhueng@gmail.com>
2023-06-14 02:24:37 +0700

    add more state `searching` and include ThreadsUtils in ConversationsThread

--- src/components/Thread.vue
+++ src/components/Thread.vue
@@ -1,20 +1,27 @@
 <template>
-  <div class="flex py-4 px-3  w-50" :class="[mode === 'normal'? 'bg-white':'']">
+  <div class="flex py-4 px-3  w-50" :class="[mode === 'normal' ? 'bg-white' : '']">
     <template v-if="mode === 'normal'">
       <img :src=conversation.participants[0].profilePic.src class="h-16 w-16 rounded-full object-cover	">
       <div class="ml-3 overflow-hidden w-full">
-        <p class=" font-bold  truncate ml-3 text-base" :class="conversation.isRead? 'text-[#B2B2B2]':'text-slate-900'">
+        <p class=" font-bold  truncate ml-3 text-base" :class="conversation.isRead ? 'text-[#B2B2B2]' : 'text-slate-900'">
           {{ conversation.participants[0].username }}</p>
         <div v-if="true" class="flex justify-between mt-1 mb-2">
-          <div class="truncate ml-3 text-sm" :class="conversation.isRead? 'text-[#B2B2B2]':'text-slate-900'">{{ conversation.lastActivity }}</div>
-          <div   class="truncate pr-3 text-sm " :class="conversation.isRead? 'text-[#B2B2B2]':'text-slate-900'">{{ updateTimeStatus }}</div>
+          <div class="truncate ml-3 text-sm" :class="conversation.isRead ? 'text-[#B2B2B2]' : 'text-slate-900'">{{
+            conversation.lastActivity }}</div>
+          <div class="truncate pr-3 text-sm " :class="conversation.isRead ? 'text-[#B2B2B2]' : 'text-slate-900'">{{
+            updateTimeStatus }}</div>
         </div>
         <UserTag />
       </div>
     </template>
-    <template v-else-if="mode === 'collapse'" >
-      <div >
+    <template v-else-if="mode === 'collapse'">
+      <div>
         <img :src=conversation.participants[0].profilePic.src class="h-14 w-14 rounded-full object-cover	">
+      </div>
+    </template>
+    <template v-else-if="mode === 'searching'">
+      <div>
+        <h1>Searching</h1>
       </div>
     </template>
   </div>
@@ -24,7 +31,7 @@
 import type { Conversation } from '@/types/conversation';
 import { onMounted, onUnmounted, ref } from 'vue';
 import UserTag from '@/components/UserTag.vue'
- const { conversation, mode } = defineProps<{
+const { conversation, mode } = defineProps<{
   conversation: Conversation
   mode: string
 }>()
--- src/views/subs/ConversationsThread.vue
+++ src/views/subs/ConversationsThread.vue
@@ -1,33 +1,26 @@
 <template>
-  <template v-if="conversationsThreadMode == 'normal'">
-    <div class="flex-1 bg-gray-300 max-w-[400px]" >
+  <div>
+    <ThreadUtils :mode="conversationsThreadMode" />
+    <div class="flex-1 bg-gray-300 max-w-[400px]">
       <div v-for="(conversation, index) in conversations('facebook')" key="conversation.conversationID" class="m-2">
         <Thread :conversation="conversation" :index="index" @click="openChat('facebook', conversation.conversationID)"
-          mode="normal" />
+          :mode="conversationsThreadMode" />
       </div>
     </div>
-  </template>
-  <template v-else-if="conversationsThreadMode == 'collapse'">
-    <div>
-      <div v-for="(conversation, index) in conversations('facebook')" key="conversation.conversationID" class="m-2">
-        <Thread :conversation="conversation" :index="index" @click="openChat('facebook', conversation.conversationID)"
-          mode="collapse" />
-      </div>
-    </div>
-  </template>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { useLivechatStore } from '@/stores/livechat';
 import { useUIStore } from '@/stores/UI';
 import Thread from '@/components/Thread.vue';
+import ThreadUtils from '@/components/ThreadsUtils.vue';
 import { storeToRefs } from 'pinia';
 const livechatStore = useLivechatStore();
 const uiStore = useUIStore();
 const { conversations } = storeToRefs(livechatStore);
 const { conversationsThreadMode } = storeToRefs(uiStore)
-//conversationsThreadMode.value = 'collapse';
-
+// conversationsThreadMode.value = 'searching'
 async function openChat(platform: string, conversationID: string) {
   await livechatStore.openChat(platform, conversationID);
 }
