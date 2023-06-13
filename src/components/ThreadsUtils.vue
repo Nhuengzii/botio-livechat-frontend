4ddf3d5218df2ffb8d739d2993d19ca8d09221c9
Nhuengzii <32nhueng@gmail.com>
2023-06-14 02:23:14 +0700

    create ThreadsUtils

---
+++ src/components/ThreadsUtils.vue
@@ -0,0 +1,19 @@
+<template>
+  <template v-if="mode == 'normal'">
+    <h1>Normal Mode</h1>
+  </template>
+  <template v-else-if="mode == 'collapse'">
+    <h1>Collapse Mode</h1>
+  </template>
+  <template v-else-if="mode == 'searching'">
+    <h1> Search Mode </h1>
+  </template>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  mode: string
+}>()
+</script>
+
+<style scoped></style>
