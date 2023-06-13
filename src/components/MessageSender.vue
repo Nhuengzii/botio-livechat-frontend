813fe232a567e834bc441e05cb6db3fa27691bdf
mahasachan <mahasachan.sa@ku.th>
2023-06-14 04:27:29 +0700

    edit input textarea

--- src/components/MessageSender.vue
+++ src/components/MessageSender.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-[1] background-d9 py-4 items-center">
+  <div class="flex flex-[1]  py-4 items-center">
     
     <!-- space -->
     <div class="flex-[1]"></div>
@@ -7,10 +7,10 @@
 
       <div class="flex-[6] flex-col">
         <div class="flex bg-stone-300 rounded-[20px] justify-around items-center text-gray-500 py-4">
-          <input type="text" placeholder="พิมพ์ข้อความ" class="inline-flex bg-stone-300 w-full ml-8"/>
+          <textarea type="text" placeholder="พิมพ์ข้อความ" class="inline-flex bg-stone-300 w-full h-4 ml-8 break-words outline-none"/>
 
           <div class="inline-flex">
-            <div class="pl-2  text-gray-500">
+            <div class="pl-2">
               <svg xmlns="http://www.w3.org/2000/svg" height="1.5em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM164.1 325.5C182 346.2 212.6 368 256 368s74-21.8 91.9-42.5c5.8-6.7 15.9-7.4 22.6-1.6s7.4 15.9 1.6 22.6C349.8 372.1 311.1 400 256 400s-93.8-27.9-116.1-53.5c-5.8-6.7-5.1-16.8 1.6-22.6s16.8-5.1 22.6 1.6zM144.4 208a32 32 0 1 1 64 0 32 32 0 1 1 -64 0zm192-32a32 32 0 1 1 0 64 32 32 0 1 1 0-64z"/></svg>
             </div>
             <div class="pl-4  text-gray-500">
@@ -33,7 +33,22 @@
 
 <script setup lang="ts">
 import { useUIStore } from '@/stores/UI';
+import { onMounted, ref } from 'vue';
 const uiStore = useUIStore()
+const inputText = ref('');
+
+const textareaRef = ref<HTMLTextAreaElement | null>(null);
+const adjustTextareaHeight = () => {
+      if (textareaRef.value) {
+        textareaRef.value.style.height = 'auto';
+        textareaRef.value.style.height = textareaRef.value.scrollHeight + 'px';
+      }
+    };
+  
+    onMounted(() => {
+      adjustTextareaHeight();
+    });
+
 </script>
 
 <style scoped>
