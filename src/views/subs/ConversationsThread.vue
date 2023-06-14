f304aece8fd4eff76b450f5caa07b1cd00e59434
Nhuengzii <32nhueng@gmail.com>
2023-06-15 02:19:56 +0700

    solve error that come with merge conflict

--- src/views/subs/ConversationsThread.vue
+++ src/views/subs/ConversationsThread.vue
@@ -1,51 +1,50 @@
 <template>
-   <div  
-  :class="[(conversationsThreadMode == 'normal'||conversationsThreadMode == 'searching')? 
-  'flex-1 max-w-[400px] bg-gray-300':'bg-white duration-300 pt-6 w-[100px]']">
-  <ThreadUtils :mode="conversationsThreadMode" />
-  <template v-if="conversationsThreadMode == 'normal'">
-    <div class="flex-1 bg-gray-300 max-w-[400px]">
-      <div v-for="(conversation, index) in conversations($route.query.platform as string)"
-        key="conversation.conversationID" class="mx-2">
-        <Thread :conversation="conversation" :index="index"
-          @click="openChat($route.query.platform as string, 
-          conversation.conversationID)" mode="normal" />
-        <hr>
-      </div>
-    </div>
-  </template>
-  <template v-else-if="conversationsThreadMode == 'collapse'">
-    <div class="bg-white ">
-      <div v-for="(conversation, index) in conversations($route.query.platform as string)"
-        key="conversation.conversationID" class="ml-3.5 my-2">
-          <Thread :conversation="conversation" :index="index"
-          @click="openChat($route.query.platform as string, conversation.conversationID)" mode="collapse" />
-       </div>
-    </div>
-  </template>
-
-  <template v-else-if="conversationsThreadMode == 'searching'">
-    <!--TAG -->
-       <div class="bg-white max-w-[400px] mx-2 pl-8 py-3 text-[#B2B2B2] text-lg font-bold">แท็ก</div> 
-      <div class="flex flex-wrap bg-white min-h-[32px] mx-2 pl-6 pr-2 ">
-        <div class="flex mx-1 my-2 px-1 bg-[#D9D9D9] rounded " v-for="(tag, index) in visibleTagLists">
-          <div class=" w-4 h-4 bg-red-500 rounded-full pr-3 mt-2 object-cover" >
-          </div>
-          <div class="text-sw px-1.5 py-1 truncate max-w-[90px]">
-              {{ tag }}
-          </div>  
+  <div :class="[(conversationsThreadMode == 'normal' || conversationsThreadMode == 'searching') ?
+    'flex-1 max-w-[400px] bg-gray-300' : 'bg-white duration-300 pt-6 w-[100px]']">
+    <ThreadUtils :mode="conversationsThreadMode" />
+    <template v-if="conversationsThreadMode == 'normal'">
+      <div class="flex-1 bg-gray-300 max-w-[400px]">
+        <div v-for="(conversation, index) in conversations($route.query.platform as string)"
+          key="conversation.conversationID" class="mx-2">
+          <Thread :conversation="conversation" :index="index" @click="livechatStore.openChat(conversation)"
+            mode="normal" />
+          <hr>
         </div>
       </div>
-    <!--End TAG-->
-    <!--User-->
-    <div class="bg-white max-w-[400px] mx-2 pl-8 py-3 text-[#B2B2B2] text-lg font-bold">ชื่อการสนทา</div> 
-    <div class="flex-1 bg-gray-300 max-w-[400px]">
-      <div v-for="(conversation, index) in conversations($route.query.platform as string)"
-        key="conversation.conversationID" class="mx-2">
-        <Thread :conversation="conversation" :index="index"
-          @click="openChat($route.query.platform as string, conversation.conversationID)" mode="searching" />
-        <hr>
- 
+    </template>
+    <template v-else-if="conversationsThreadMode == 'collapse'">
+      <div class="bg-white ">
+        <div v-for="(conversation, index) in conversations($route.query.platform as string)"
+          key="conversation.conversationID" class="ml-3.5 my-2">
+          <Thread :conversation="conversation" :index="index" @click="livechatStore.openChat(conversation)"
+            mode="collapse" />
+        </div>
+      </div>
+    </template>
+
+    <template v-else-if="conversationsThreadMode == 'searching'">
+      <!--TAG -->
+      <div class="bg-white max-w-[400px] mx-2 pl-8 py-3 text-[#B2B2B2] text-lg font-bold">แท็ก</div>
+      <div class="flex flex-wrap bg-white min-h-[32px] mx-2 pl-6 pr-2 ">
+        <div class="flex mx-1 my-2 px-1 bg-[#D9D9D9] rounded " v-for="(tag, index) in ['yay']">
+          <div class=" w-4 h-4 bg-red-500 rounded-full pr-3 mt-2 object-cover">
+          </div>
+          <div class="text-sw px-1.5 py-1 truncate max-w-[90px]">
+            {{ tag }}
+          </div>
+        </div>
+      </div>
+      <!--End TAG-->
+      <!--User-->
+      <div class="bg-white max-w-[400px] mx-2 pl-8 py-3 text-[#B2B2B2] text-lg font-bold">ชื่อการสนทา</div>
+      <div class="flex-1 bg-gray-300 max-w-[400px]">
+        <div v-for="(conversation, index) in conversations($route.query.platform as string)"
+          key="conversation.conversationID" class="mx-2">
+          <Thread :conversation="conversation" :index="index" @click="livechatStore.openChat(conversation)"
+            mode="searching" />
+          <hr>
+
+        </div>
       </div>
     </template>
 
