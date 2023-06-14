e2f06a2ee0509b7f587cf3fc93434f184200b134
Nhuengzii <32nhueng@gmail.com>
2023-06-15 02:27:47 +0700

    comment foreign things

--- src/components/ThreadsUtils.vue
+++ src/components/ThreadsUtils.vue
@@ -1,75 +1,80 @@
 <template >
-  <template v-if="mode == 'normal'||mode== 'searching'">
- 
-    <div class="bg-[#EAEAEA]  mx-2 min-w-[320px] duration-500" :class="[query? 'h-32' :'h-40']" >
+  <template v-if="mode == 'normal' || mode == 'searching'">
+    <div class="bg-[#EAEAEA]  mx-2 min-w-[320px] duration-500" :class="[query ? 'h-32' : 'h-40']">
       <div class="bg-white h-36 my-4">
         <div v-if="mode == 'normal'" class="flex pt-5 ml-5 justify-between">
-            <div class="flex">ข้อความทั้งหมด <div class=" px-2  bg-[#D9D9D9] rounded-full ml-3 font-bold text-base">+999</div>
-            </div>
-            <div class="flex bg-red-400">
-               <button @click="uiStore.conversationsThreadMode='collapse'">
-                555
-               </button>
+          <div class="flex">ข้อความทั้งหมด <div class=" px-2  bg-[#D9D9D9] rounded-full ml-3 font-bold text-base">+999
             </div>
           </div>
-      <div class=" flex  justify-around py-[1rem] px-2" :class="query? '':'pl-5 '">
-            <div v-if="query"  :class="[query? 'mt-1.5 ':'']">
-                <button type="button" @click="{ query = false; uiStore.conversationsThreadMode='normal'; messageSaersh = '' }"
-                    class="text-white   hover:bg-gray-100  p-2 rounded-full inline-flex items-center  ml-2">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="0 0 448 512">
-                      <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"/>
-                    </svg>
-                    <span class="sr-only">Icon description</span>
-                </button>
-              </div>
-              <div class="relative w-full mr-4 duration-500" :class="[query? 'mt-2':'mt-2']">
-                <div v-if="!query"  class="flex absolute  rounded-full z-10 inset-y-0 left-0 items-center pl-3 pointer-events-none ml-2" >
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6" >
-                      <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-                  </svg>
-                </div>
-                <input type="text"   @click="{ query = true; uiStore.conversationsThreadMode='searching';}" :class="query? '':' rounded-full'" 
-                    class="ml-2 bg-[#D9D9D9]  border border-gray-300 text-gray-900  outline-none   block w-full pl-10 p-2.5   dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    :placeholder="query? 'ค้นหา':' ค้นหาจากชื่อ หรือ แท็ก'" v-model="messageSaersh">
- 
-              </div>
+          <div class="flex bg-red-400">
+            <button @click="uiStore.conversationsThreadMode = 'collapse'">
+              555
+            </button>
+          </div>
+        </div>
+        <div class=" flex  justify-around py-[1rem] px-2" :class="query ? '' : 'pl-5 '">
+          <div v-if="query" :class="[query ? 'mt-1.5 ' : '']">
+            <button type="button" @click="{ query = false; uiStore.conversationsThreadMode = 'normal'; messageSaersh = '' }"
+              class="text-white   hover:bg-gray-100  p-2 rounded-full inline-flex items-center  ml-2">
+              <svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="0 0 448 512">
+                <path
+                  d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z" />
+              </svg>
+              <span class="sr-only">Icon description</span>
+            </button>
+          </div>
+          <div class="relative w-full mr-4 duration-500" :class="[query ? 'mt-2' : 'mt-2']">
+            <div v-if="!query"
+              class="flex absolute  rounded-full z-10 inset-y-0 left-0 items-center pl-3 pointer-events-none ml-2">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                  d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+              </svg>
+            </div>
+            <input type="text" @click="{ query = true; uiStore.conversationsThreadMode = 'searching'; }"
+              :class="query ? '' : ' rounded-full'"
+              class="ml-2 bg-[#D9D9D9]  border border-gray-300 text-gray-900  outline-none   block w-full pl-10 p-2.5   dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              :placeholder="query ? 'ค้นหา' : ' ค้นหาจากชื่อ หรือ แท็ก'" v-model="messageSaersh">
+
+          </div>
         </div>
       </div>
     </div>
   </template>
   <template v-else-if="mode == 'collapse'">
-    <div >
-      <div  >
-        <button @click="uiStore.conversationsThreadMode='normal'" class="p-5 ml-6 mb-1 ">
+    <div>
+      <div>
+        <button @click="uiStore.conversationsThreadMode = 'normal'" class="p-5 ml-6 mb-1 ">
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 320 512">
-            <path d="M278.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L210.7 256 73.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z"/>
+            <path
+              d="M278.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L210.7 256 73.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z" />
           </svg>
         </button>
       </div>
       <div>
-        <button @click="uiStore.conversationsThreadMode='searching'" class="p-4 ml-6 mb-3 bg-gray-300 rounded-full">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6" >
-              <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+        <button @click="uiStore.conversationsThreadMode = 'searching'" class="p-4 ml-6 mb-3 bg-gray-300 rounded-full">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+            class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round"
+              d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
           </svg>
         </button>
       </div>
     </div>
   </template>
- 
 </template>
 
 <script setup lang="ts">
-import {ref} from 'vue'
+import { ref } from 'vue'
 import { useUIStore } from '@/stores/UI';
 const uiStore = useUIStore();
- let messageSaersh = ref("");
- const query = ref(false);
+let messageSaersh = ref("");
+const query = ref(false);
 defineProps<{
   mode: string
 }>()
 </script>
  
-<style scoped>
-
-</style>
+<style scoped></style>
  
--- src/views/subs/ConversationsThread.vue
+++ src/views/subs/ConversationsThread.vue
@@ -1,79 +1,62 @@
 <template>
+  <h1>{{ conversationsThreadMode }}</h1>
   <div :class="[(conversationsThreadMode == 'normal' || conversationsThreadMode == 'searching') ?
     'flex-1 max-w-[400px] bg-gray-300' : 'bg-white duration-300 pt-6 w-[100px]']">
     <ThreadUtils :mode="conversationsThreadMode" />
-    <template v-if="conversationsThreadMode == 'normal'">
-      <div class="flex-1 bg-gray-300 max-w-[400px]">
-        <div v-for="(conversation, index) in conversations($route.query.platform as string)"
-          key="conversation.conversationID" class="mx-2">
-          <Thread :conversation="conversation" :index="index" @click="livechatStore.openChat(conversation)"
-            mode="normal" />
-          <hr>
-        </div>
+    <div class="flex-1 bg-gray-300 max-w-[400px]">
+      <div v-for="(conversation, index) in conversations($route.query.platform as string)"
+        key="conversation.conversationID" class="mx-2">
+        <Thread :conversation="conversation" :index="index" @click="livechatStore.openChat(conversation)"
+          :mode="conversationsThreadMode" />
+        <hr>
       </div>
-    </template>
-    <template v-else-if="conversationsThreadMode == 'collapse'">
-      <div class="bg-white ">
-        <div v-for="(conversation, index) in conversations($route.query.platform as string)"
-          key="conversation.conversationID" class="ml-3.5 my-2">
-          <Thread :conversation="conversation" :index="index" @click="livechatStore.openChat(conversation)"
-            mode="collapse" />
-        </div>
-      </div>
-    </template>
+    </div>
 
-    <template v-else-if="conversationsThreadMode == 'searching'">
-      <!--TAG -->
-      <div class="bg-white max-w-[400px] mx-2 pl-8 py-3 text-[#B2B2B2] text-lg font-bold">แท็ก</div>
-      <div class="flex flex-wrap bg-white min-h-[32px] mx-2 pl-6 pr-2 ">
-        <div class="flex mx-1 my-2 px-1 bg-[#D9D9D9] rounded " v-for="(tag, index) in ['yay']">
-          <div class=" w-4 h-4 bg-red-500 rounded-full pr-3 mt-2 object-cover">
-          </div>
-          <div class="text-sw px-1.5 py-1 truncate max-w-[90px]">
-            {{ tag }}
-          </div>
-        </div>
-      </div>
-      <!--End TAG-->
-      <!--User-->
-      <div class="bg-white max-w-[400px] mx-2 pl-8 py-3 text-[#B2B2B2] text-lg font-bold">ชื่อการสนทา</div>
-      <div class="flex-1 bg-gray-300 max-w-[400px]">
-        <div v-for="(conversation, index) in conversations($route.query.platform as string)"
-          key="conversation.conversationID" class="mx-2">
-          <Thread :conversation="conversation" :index="index" @click="livechatStore.openChat(conversation)"
-            mode="searching" />
-          <hr>
-
-        </div>
-      </div>
-    </template>
-
-    <template v-else-if="conversationsThreadMode == 'searching'">
-      <!--TAG -->
-      <div class="bg-white max-w-[400px] mx-2 pl-2 py-3">แท็ก</div>
-      <div class="flex flex-wrap bg-white min-h-[32px] mx-2 pl-3 pr-2 ">
-        <div class="flex mx-1 my-2 px-1 bg-[#D9D9D9] rounded " v-for="(tag, index) in ['yay', 'atf']">
-          <div class=" w-4 h-4 bg-red-500 rounded-full pr-3 mt-2 object-cover">
-          </div>
-          <div class="text-sw px-1.5 py-1 truncate max-w-[90px]">
-            {{ tag }}
-          </div>
-        </div>
-      </div>
-      <!--End TAG-->
-      <!--User-->
-      <div class="bg-white max-w-[400px] mx-2 pl-2 py-3">ชื่อการสนทา</div>
-      <div class="flex-1 bg-gray-300 max-w-[400px]">
-        <div v-for="(conversation, index) in conversations($route.query.platform as string)"
-          key="conversation.conversationID" class="mx-2">
-          <Thread :conversation="conversation" :index="index" @click="livechatStore.openChat(conversation)"
-            mode="searching" />
-          <hr>
-
-        </div>
-      </div>
-      <!--End User-->
-    </template>
+    <!-- <template v-if="conversationsThreadMode == 'searching'"> -->
+    <!--   <div class="bg-white max-w-[400px] mx-2 pl-8 py-3 text-[#B2B2B2] text-lg font-bold">แท็ก</div> -->
+    <!--   <div class="flex flex-wrap bg-white min-h-[32px] mx-2 pl-6 pr-2 "> -->
+    <!--     <div class="flex mx-1 my-2 px-1 bg-[#D9D9D9] rounded " v-for="(tag, index) in ['yay']"> -->
+    <!--       <div class=" w-4 h-4 bg-red-500 rounded-full pr-3 mt-2 object-cover"> -->
+    <!--       </div> -->
+    <!--       <div class="text-sw px-1.5 py-1 truncate max-w-[90px]"> -->
+    <!--         {{ tag }} -->
+    <!--       </div> -->
+    <!--     </div> -->
+    <!--   </div> -->
+    <!--   <div class="bg-white max-w-[400px] mx-2 pl-8 py-3 text-[#B2B2B2] text-lg font-bold">ชื่อการสนทา</div> -->
+    <!--   <div class="flex-1 bg-gray-300 max-w-[400px]"> -->
+    <!--     <div v-for="(conversation, index) in conversations($route.query.platform as string)" -->
+    <!--       key="conversation.conversationID" class="mx-2"> -->
+    <!--       <Thread :conversation="conversation" :index="index" @click="livechatStore.openChat(conversation)" -->
+    <!--         mode="searching" /> -->
+    <!--       <hr> -->
+    <!---->
+    <!--     </div> -->
+    <!--   </div> -->
+    <!-- </template> -->
+    <!---->
+    <!-- <template v-else-if="conversationsThreadMode == 'searching'"> -->
+    <!--   <div class="bg-white max-w-[400px] mx-2 pl-2 py-3">แท็ก</div> -->
+    <!--   <div class="flex flex-wrap bg-white min-h-[32px] mx-2 pl-3 pr-2 "> -->
+    <!--     <div class="flex mx-1 my-2 px-1 bg-[#D9D9D9] rounded " v-for="(tag, index) in ['yay', 'atf']"> -->
+    <!--       <div class=" w-4 h-4 bg-red-500 rounded-full pr-3 mt-2 object-cover"> -->
+    <!--       </div> -->
+    <!--       <div class="text-sw px-1.5 py-1 truncate max-w-[90px]"> -->
+    <!--         {{ tag }} -->
+    <!--       </div> -->
+    <!--     </div> -->
+    <!--   </div> -->
+    <!--   <div class="bg-white max-w-[400px] mx-2 pl-2 py-3">ชื่อการสนทา</div> -->
+    <!--   <div class="flex-1 bg-gray-300 max-w-[400px]"> -->
+    <!--     <div v-for="(conversation, index) in conversations($route.query.platform as string)" -->
+    <!--       key="conversation.conversationID" class="mx-2"> -->
+    <!--       <Thread :conversation="conversation" :index="index" @click="livechatStore.openChat(conversation)" -->
+    <!--         mode="searching" /> -->
+    <!--       <hr> -->
+    <!---->
+    <!--     </div> -->
+    <!--   </div> -->
+    <!-- </template> -->
   </div>
 </template>
 
