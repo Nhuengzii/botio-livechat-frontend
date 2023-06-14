b1cb6843dd076ce06641f7967090f10e462a54c4
peebaKU <baramee.254450@gmail.com>
2023-06-15 02:10:09 +0700

    Add saerch v3

--- src/views/Livechat.vue
+++ src/views/Livechat.vue
@@ -1,7 +1,5 @@
  <template>
-  <header class="pt-2 pb-3 px-3">
-    <h1>THIS is LiveChat view</h1>
-  </header>
+ 
   <main class="flex h-[calc(100vh-50px)]">
     <ConversationsThread />
     <Chats />
