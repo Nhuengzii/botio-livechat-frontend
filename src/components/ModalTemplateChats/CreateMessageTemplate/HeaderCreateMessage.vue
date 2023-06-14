ded846415d2d0a265bb0e6b319826e13fad355ec
mahasachan <mahasachan.sa@ku.th>
2023-06-15 03:23:43 +0700

    init layout create message template

---
+++ src/components/ModalTemplateChats/CreateMessageTemplate/HeaderCreateMessage.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="flex justify-center">
+        <header class="flex">
+            <div class="bg-blue-dark py-4 px-12">
+                <h3 class="text-lg text-white font-semibold">สร้างเทมเพลตข้อความ</h3>
+            </div>
+        </header>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { useUIStore } from '@/stores/UI';
+const uiStore = useUIStore()
+</script>
+
+<style scoped>
+.background-d9-300 {
+    background-color: rgba(217, 217, 217, 0.3);
+}
+
+.background-d9-250 {
+    background-color: rgba(217, 217, 217, 0.25);
+}
+
+.color-d9 {
+    color: rgba(217, 217, 217, 1);
+}
+
+.color-b2 {
+    color: #B2B2B2;
+}
+
+.bg-ea {
+    background-color: #EAEAEA;
+}
+
+.bg-ea-80 {
+    background-color: #eaeaeacc;
+}
+
+.bg-blue-dark {
+    background-color: #394867;
+}
+
+.bg-blue-dark-light {
+    background-color: #39486788;
+}
+
+.bg-blue-dark-green-90 {
+    background-color: #00aab3e5;
+}
+
+.color-dark-green {
+    color: #00ABB3;
+}
+</style>
