0813b5a8d02066df6559ff549b3cf74c962a6200
Nhuengzii <32nhueng@gmail.com>
2023-06-20 02:45:49 +0700

    reimplement livechat store

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,6 @@ import Navbar from './components/Navbar.vue';
 import { useLivechatStore } from './stores/livechat';
 import type { Message } from './types/message';
 
-const livechatStore = useLivechatStore();
-livechatStore.botioLivechat.websocketConnection.onmessage = (event) => {
-  console.log(event)
-  console.log(JSON.stringify(event, null, 2))
-  const data: { action: string, data: Message } = JSON.parse(event.data);
-  console.log(data)
-  if (data.action === 'relay') {
-    livechatStore.addReceivedMessage(data.data);
-  }
-};
-
 </script>
 
 <template >
