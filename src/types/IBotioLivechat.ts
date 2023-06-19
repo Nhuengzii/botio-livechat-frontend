ee19e1c5f59a0215a0451cb4aa4f79223c7d6407
Nhuengzii <32nhueng@gmail.com>
2023-06-20 00:08:50 +0700

    update IBotioLivechat

---
+++ src/types/IBotioLivechat.ts
@@ -0,0 +1,15 @@
+import type { Conversation } from "./conversation"
+import type { Message } from "./message"
+
+interface IBotioLivechat {
+  botioRestApiUrl: string
+  botioWebsocketApiUrl: string
+  listConversation: (platform: string, pageID: string) => Promise<Conversation[]>
+  getConversation: (platform: string, pageID: string, conversationId: string) => Promise<Conversation | null>
+  listMessage: (platform: string, pageID: string, conversationId: string) => Promise<Message[]>
+  getMessage: (platform: string, pageID: string, conversationId: string) => Promise<Message | null>
+  sendTextMessage: (platform: string, conversationId: string, pageID: string, psid: string, text: string) => Promise<Message | null>
+  broadcastMessage: (platform: string, pageID: string, text: string) => Promise<Message | null>
+}
+
+export default IBotioLivechat;
