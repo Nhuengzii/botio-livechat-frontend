95c25e502b9df1e93f44adc06b01550a0862a7c0
Nhuengzii <32nhueng@gmail.com>
2023-06-20 20:34:24 +0700

    make shop display component

---
+++ src/components/ShopDisplay.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="rounded-lg flex bg-sky-50 justify-between">
+    <img
+      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTccLkwQM4NOYfjcpSP566OJEdPR2e20hHksbSugOD4w0QoShSbmTAPYiGFjCqTi5f64zg&usqp=CAU"
+      class="object-cover rounded-full w-12 h-12" alt="">
+    <h1 class="font-bold text-m"> NamePage</h1>
+    <button class="bg-red-500 p-1 rounded-lg">เปลี่ยนเพจ</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped></style>
