776397c98fb07ca65380010ccb30eaa96ba64193
Nhuengzii <32nhueng@gmail.com>
2023-06-20 21:17:46 +0700

    decorate shopDisplay

--- src/components/ShopDisplay.vue
+++ src/components/ShopDisplay.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="rounded-lg flex bg-sky-50 justify-between">
+  <div class="rounded-lg flex justify-between items-center bg-white py-4">
     <img
       src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTccLkwQM4NOYfjcpSP566OJEdPR2e20hHksbSugOD4w0QoShSbmTAPYiGFjCqTi5f64zg&usqp=CAU"
-      class="object-cover rounded-full w-12 h-12" alt="">
-    <h1 class="font-bold text-m"> NamePage</h1>
-    <button class="bg-red-500 p-1 rounded-lg">เปลี่ยนเพจ</button>
+      class="object-cover rounded-full w-14 h-14 ml-5 my-2" alt="">
+    <h1 class="font-bold text-m">NamePage</h1>
+    <button class="bg-red-500 p-1 rounded-3xl justify-self-end text-white px-3">เปลี่ยนเพจ</button>
   </div>
 </template>
 
