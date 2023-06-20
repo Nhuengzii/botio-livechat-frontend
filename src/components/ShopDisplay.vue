595749ca9ac7fec3a15547b9f661872484162abc
Nhuengzii <32nhueng@gmail.com>
2023-06-20 21:29:11 +0700

    make ShopDisplay more rounded

--- src/components/ShopDisplay.vue
+++ src/components/ShopDisplay.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="rounded-lg flex justify-between items-center bg-white py-4">
+  <div class="rounded-3xl flex justify-between items-center bg-white py-4">
     <img
       src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTccLkwQM4NOYfjcpSP566OJEdPR2e20hHksbSugOD4w0QoShSbmTAPYiGFjCqTi5f64zg&usqp=CAU"
       class="object-cover rounded-full w-14 h-14 ml-5 my-2" alt="">
