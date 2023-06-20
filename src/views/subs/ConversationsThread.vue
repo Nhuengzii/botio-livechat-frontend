bdb999297a4157e28c323f2da0f0ea898c243a3e
Nhuengzii <32nhueng@gmail.com>
2023-06-20 20:53:54 +0700

    clean conversationThread

--- src/views/subs/ConversationsThread.vue
+++ src/views/subs/ConversationsThread.vue
@@ -1,29 +1,12 @@
 <template>
-  <!-- <button @click="burstConversation">ASSS</button> -->
-  <div class="mt-3 ml-3" :class="[(conversationsThreadMode == 'normal' || conversationsThreadMode == 'searching') ?
-    'flex-1 max-w-[400px] bg-d9-30' : 'bg-white duration-300 pt-6 w-[100px]']">
-    <template v-if="(conversationsThreadMode == 'normal'
-      || conversationsThreadMode == 'searching')
-      && isLoading">
-      <div class="flex-1 max-w-[400px] max-h-[700px] ">
-        <div class="flex flex-col items-center justify-center h-full">
-          <ThreadSkeleton :num-skeletons="6" />
-        </div>
+  <div v-bind="containerProps" class="bg-red-500">
+    <div v-bind="wrapperProps">
+      <div v-for="({ data }, index) in list" :key="data.conversationID">
+        <Thread :conversation="data" :index="index" @click="livechatStore.openChat(data.platform, data.conversationID)"
+          :show-platform="$route.query.platform == 'centralized'" :mode="conversationsThreadMode"
+          class="hover:bg-[#eeeeee]" />
       </div>
-    </template>
-    <template v-else>
-      <div v-bind="containerProps" class="flex-1 max-w-[400px] max-h-[700px] "
-        :class="[conversationsThreadMode == 'collapse' ? 'ml-3.5 no-scrollbar' : 'mx-4 ']">
-        <div v-bind="wrapperProps">
-          <div v-for="({ data }, index) in list" :key="data.conversationID"
-            :class="[conversationsThreadMode == 'collapse' ? '' : 'mx-2']">
-            <Thread :conversation="data" :index="index"
-              @click="livechatStore.openChat(data.platform, data.conversationID)"
-              :show-platform="$route.query.platform == 'centralized'" :mode="conversationsThreadMode" />
-          </div>
-        </div>
-      </div>
-    </template>
+    </div>
   </div>
 </template>
 
@@ -31,8 +14,6 @@
 import { useLivechatStore } from '@/stores/livechat';
 import { useUIStore } from '@/stores/UI';
 import Thread from '@/components/Thread.vue';
-import ThreadUtils from '@/components/ThreadsUtils.vue';
-import ThreadSkeleton from '@/components/ThreadSkeleton.vue'
 import { storeToRefs } from 'pinia';
 import { onMounted, ref, watch } from 'vue';
 import { useRoute } from 'vue-router';
@@ -40,7 +21,7 @@ import { useVirtualList } from '@vueuse/core';
 import { computed } from '@vue/reactivity';
 const livechatStore = useLivechatStore();
 const uiStore = useUIStore();
-const { botioLivechat, conversations } = storeToRefs(livechatStore);
+const { conversations } = storeToRefs(livechatStore);
 const { conversationsThreadMode } = storeToRefs(uiStore)
 const isLoading = ref(false);
 const route = useRoute()
