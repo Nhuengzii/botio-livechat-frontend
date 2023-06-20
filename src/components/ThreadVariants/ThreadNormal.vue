ebe3b4c2a9b7ff37aab2a31d4aa4b2a6c5de8e28
Nhuengzii <32nhueng@gmail.com>
2023-06-20 23:07:02 +0700

    remake Thread

---
+++ src/components/ThreadVariants/ThreadNormal.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="flex">
+    <ThreadAvatar :profile-pic="conversation.participants[0].profilePic.src" :show-platform="showPlatform"
+      :platform="conversation.platform" />
+    <div class="flex-1 justify-between mx-3">
+      <h1 :class="conversation.isRead ? '' : 'font-bold'">{{ conversation.participants[0].username }}
+      </h1>
+      <p class="truncate" :class="conversation.isRead ? '' : 'font-bold'"> {{ conversation.lastActivity }}</p>
+    </div>
+    <h1>{{ updateTimeStatus }}</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { Conversation } from '@/types/conversation';
+import ThreadAvatar from '@/components/ThreadVariants/ThreadAvatar.vue'
+
+const { conversation } = defineProps<{
+  conversation: Conversation,
+  showPlatform: boolean
+  updateTimeStatus: string
+}>()
+</script>
+
+<style scoped></style>
