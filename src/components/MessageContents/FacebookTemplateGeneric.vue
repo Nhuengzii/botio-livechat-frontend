3eff3f57f98d29fc5f250cdfa98b539d516a4e33
Nhuengzii <32nhueng@gmail.com>
2023-06-21 14:13:33 +0700

    update FacebookTemplateGeneric

--- src/components/MessageContents/FacebookTemplateGeneric.vue
+++ src/components/MessageContents/FacebookTemplateGeneric.vue
@@ -1,15 +1,17 @@
 <template>
-  <div class="rounded-t-3xl">
-    <img :src="template.elements[0].image_url" class="object-contain rounded-t-3xl" alt="">
-  </div>
-  <div class="rounded-b-3xl bg-gray-200 pb-2 pt-1">
-    <div class="ml-3">
-      <h1 class="font-bold">{{ template.elements[0].title }}</h1>
-      <h2>{{ template.elements[0].subtitle }}</h2>
+  <div>
+    <div class="rounded-t-3xl h-64 overflow-hidden">
+      <img :src="template.elements[0].image_url" class="object-contain rounded-t-3xl" alt="">
     </div>
-    <div class="flex justify-center bg-white mx-3 my-3 py-1 rounded-xl">
-      <a :href="template.elements[0].buttons[0].url" target="_blank" class="">{{
-        template.elements[0].buttons[0].title }}</a>
+    <div class="rounded-b-3xl bg-gray-200 pb-2 pt-1">
+      <div class="ml-3">
+        <h1 class="font-bold">{{ template.elements[0].title }}</h1>
+        <h2>{{ template.elements[0].subtitle }}</h2>
+      </div>
+      <div class="flex justify-center bg-white mx-3 my-3 py-1 rounded-xl">
+        <a :href="template.elements[0].buttons[0].url" target="_blank" class="">{{
+          template.elements[0].buttons[0].title }}</a>
+      </div>
     </div>
   </div>
 </template>
