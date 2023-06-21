b7f12f31008c4d00dff6d3477e1cfff3c5ba7e6f
Nhuengzii <32nhueng@gmail.com>
2023-06-21 21:13:28 +0700

    update generic more robust

--- src/components/MessageContents/InstagramTemplateGeneric.vue
+++ src/components/MessageContents/InstagramTemplateGeneric.vue
@@ -1,6 +1,20 @@
 <template>
-  <div>
-    <!-- <h1>{{ message.attachments }}</h1> -->
+  <div class="flex">
+    <div v-for="element in template.elements">
+      <div class="rounded-t-3xl h-64 overflow-hidden">
+        <img :src="element.image_url" class="object-contain rounded-t-3xl" alt="">
+      </div>
+      <div class="rounded-b-3xl bg-gray-200 pb-2 pt-1">
+        <div class="ml-3">
+          <h1 class="font-bold">{{ element.title }}</h1>
+          <h2>{{ element.subtitle }}</h2>
+        </div>
+        <div class="flex justify-center bg-white mx-3 my-3 py-1 rounded-xl hover:bg-gray-100"
+          v-for="button in element.buttons">
+          <a :href="button.url" target="_blank" class="">{{ button.title }}</a>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,8 +23,24 @@ import type { Message } from "@/types/message"
 const { message } = defineProps<{
   message: Message
 }>()
-const template = JSON.parse(message.attachments[0].payload.src)
-// console.log(JSON.stringify(template, null, 2))
+type InstagramTemplateGeneric = {
+  elements: {
+    buttons: {
+      title: string,
+      type: string,
+      url: string
+    }[],
+    default_action: {
+      type: string,
+      url: string
+    },
+    image_url: string,
+    subtitle: string,
+    title: string
+  }[]
+}
+const template: InstagramTemplateGeneric = JSON.parse(message.attachments[0].payload.src)
+console.log(JSON.stringify(template, null, 2))
 
 </script>
 
--- src/components/MessageContents/InstagramTemplateProduct.vue
+++ src/components/MessageContents/InstagramTemplateProduct.vue
@@ -1,16 +1,18 @@
 <template>
-  <div>
-    <div class="rounded-t-3xl h-64 overflow-hidden">
-      <img :src="template.generic.elements[0].image_url" class="object-contain rounded-t-3xl" alt="">
-    </div>
-    <div class="rounded-b-3xl bg-gray-200 pb-2 pt-1">
-      <div class="ml-3">
-        <h1 class="font-bold">{{ template.generic.elements[0].title }}</h1>
-        <h2>{{ template.generic.elements[0].subtitle }}</h2>
+  <div class="flex">
+    <div v-for="element in template.generic.elements">
+      <div class="rounded-t-3xl h-64 overflow-hidden">
+        <img :src="element.image_url" class="object-contain rounded-t-3xl" alt="">
       </div>
-      <div class="flex justify-center bg-white mx-3 my-3 py-1 rounded-xl hover:bg-gray-100">
-        <a :href="button.url" v-for="button in template.generic.elements[0].buttons" target="_blank" class="">{{
-          button.title }}</a>
+      <div class="rounded-b-3xl bg-gray-200 pb-2 pt-1">
+        <div class="ml-3">
+          <h1 class="font-bold">{{ element.title }}</h1>
+          <h2>{{ element.subtitle }}</h2>
+        </div>
+        <div class="flex justify-center bg-white mx-3 my-3 py-1 rounded-xl hover:bg-gray-100"
+          v-for="button in element.buttons">
+          <a :href="button.url" target="_blank" class="">{{ button.title }}</a>
+        </div>
       </div>
     </div>
   </div>
