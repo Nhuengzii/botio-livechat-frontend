377dcb198023d4c1b703b13d89232513893ecc85
Nhuengzii <32nhueng@gmail.com>
2023-06-22 02:24:00 +0700

    implement LineTemplateConfirm

--- src/components/MessageContents/LineTemplatConfirm.vue
+++ src/components/MessageContents/LineTemplatConfirm.vue
@@ -1,6 +1,12 @@
 <template>
-  <div>
-    <h1>{{ message.attachments[0] }}</h1>
+  <div class="bg-gray-50 pt-2 rounded-lg">
+    <h1 class=""> {{ payload.text }}</h1>
+    <hr>
+    <div class="flex justify-evenly">
+      <div v-for="action in payload.actions" class="p-3 hover:bg-gray-100" :key="action.label">
+        <a :href="action.uri" class="text-blue-500" target="_blank">{{ action.label }}</a>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,6 +15,19 @@ import type { Message } from '@/types/message'
 const { message } = defineProps<{
   message: Message
 }>()
+type LineTemplateConfirm = {
+  attachmentType: string,
+  payload: {
+    altText: string,
+    text: string,
+    actions: {
+      label: string,
+      uri: string
+    }[]
+  }
+}
+const template = JSON.parse(message.attachments[0].payload.src)
+const { payload } = template;
 </script>
 
 <style scoped></style>
