0b47c4fc3266771265329367bdb8a36ebd3098ae
Nhuengzii <32nhueng@gmail.com>
2023-06-22 03:03:29 +0700

    implement LineTemplateImageCarousel

--- src/components/MessageContents/LineTemplateImageCarousel.vue
+++ src/components/MessageContents/LineTemplateImageCarousel.vue
@@ -1,14 +1,36 @@
 <template>
-  <div>
-     <h1 > {{ message.attachments[0] }} </h1>
+  <div class="flex">
+    <div v-for="column in payload.columns" class="m-4">
+      <div class="relative">
+        <img :src="column.imageUrl" class="object-cover rounded-lg">
+        <a :href="column.action.uri" class="absolute inset-x-0 bottom-1 bg-black rounded-3xl text-white text-center"
+          target="_blank">{{
+            column.action.label }}</a>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import type { Message } from '@/types/message'
+type LineTemplateImageCarousel = {
+  payload: {
+    altText: string,
+    columns: {
+      imageUrl: string,
+      action: {
+        label: string,
+        uri: string
+      }
+    }[]
+  },
+}
 const { message } = defineProps<{
   message: Message
 }>()
+const template: LineTemplateImageCarousel = JSON.parse(message.attachments[0].payload.src)
+const { payload } = template;
+console.log(JSON.stringify(template, null, 2))
 </script>
 
 <style scoped></style>
