bfa61ed03d32ef9ac8d175a2f7e9e768270ae3ab
peebaKU <baramee.254450@gmail.com>
2023-06-22 14:51:37 +0700

    add function switch saerch's mode

--- src/components/ThreadsUtils.vue
+++ src/components/ThreadsUtils.vue
@@ -18,25 +18,24 @@
     </div>
   </template>
   <template v-else-if="mode == 'searching'">
-    <div>
-      <div class="flex items-center ">
-        <div class="pl-8" @click="() => { uiStore.conversationsThreadMode = 'normal' } ">
+    <div class="bg-white  mx-2 mt-2">
+      <div class="flex items-center">
+        <div class="pl-8 pt-5" @click="() => { uiStore.conversationsThreadMode = 'normal' } ">
           <font-awesome-icon :icon="['fas', 'arrow-left']" size="xl" />
         </div>
-        <div class="flex items-center w-full pr-4 pl-5 mr-8">
+        <div class="flex items-center w-full pr-4 pl-2 mr-1 mt-5">
           <input type="text" 
-          class="ml-2 bg-[#D9D9D9]  border border-gray-300 text-gray-900  outline-none   block w-full pl-10 p-1   dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          :placeholder="querying ? 'ค้นหา' : ' ค้นหาจากชื่อ หรือ แท็ก'" v-model="query">
-          <div class="  "
+          class="ml-2 bg-[#D9D9D9]  border border-gray-300 text-gray-900  outline-none   block w-full pl-2 p-1   dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="ค้นหา" v-model="query">
+          <div class="ml-2"  @click="() => {query=  ''} "
             :class="[querying ? '' : 'z-50']">
             <font-awesome-icon :icon="['fas', 'xmark']" size="xl" />
           </div>
         </div>
       </div>
       <div class="flex justify-between px-4 py-2">
-        <h1>ทั้งหมด</h1>
-        <h1>แชทข้อความ</h1>
-        <h1>ชื่อสนทนา</h1>
+        <button @click="() => { searchMode = 'by-message'}">แชทข้อความ</button>
+        <button @click="() => { searchMode = 'by-name' }">ชื่อสนทนา</button>
       </div>
       <div v-for="conversation in searchResult" :key="conversation.conversationID">
         <Thread :conversation="conversation" :show-platform="true" mode="searching" />
@@ -65,7 +64,6 @@ const querying = ref(false)
 
 watch(query, (value) => {
   if (value.length > 0) {
-    querying.value = true
     if (searchMode.value === 'by-name') {
       livechatStore.searchConversationByName('facebook', query.value).then((result) => {
         searchResult.value = result
