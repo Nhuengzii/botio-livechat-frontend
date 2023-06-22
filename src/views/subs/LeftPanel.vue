1b5b5a033ab8c1a2eb3f9b7bacaf4312b5676bfd
Nhuengzii <32nhueng@gmail.com>
2023-06-22 18:57:01 +0700

    make ThreadsUtils depend on platform

--- src/views/subs/LeftPanel.vue
+++ src/views/subs/LeftPanel.vue
@@ -1,26 +1,26 @@
-<template> 
-    <div class="flex flex-col justify-between bg-[#D9D9D94c]  min-w-[320px] max-w-[350px]">
-      <div>
-        <template v-if="conversationsThreadMode == 'normal'">
-          <div class="px-2">
-            <ShopDisplay />
-          </div>        
-               <PlatformSelector class="pt-4  mx-2" />
- 
-              <ThreadsUtils mode="normal" class="mx-2" />
-              <ConversationThred class="px-2 no-scrollbar" />
-        </template>
-        <template v-else>
-          <div>
-            <ThreadsUtils mode="searching" />
-          </div>
-        </template>
-      </div>
-      <div class="flex w-full p-5 bg-slate-700 justify-center text-[#FFD95A] text-[16px]">
-        Botio X CPE
-      </div>
+<template>
+  <div class="flex flex-col justify-between bg-[#D9D9D94c]  min-w-[320px] max-w-[350px]">
+    <div>
+      <template v-if="conversationsThreadMode == 'normal'">
+        <div class="px-2">
+          <ShopDisplay />
+        </div>
+        <PlatformSelector class="pt-4  mx-2" />
+
+        <ThreadsUtils mode="normal" class="mx-2" :platform="$route.query.platform as string" />
+        <ConversationThred class="px-2 no-scrollbar" />
+      </template>
+      <template v-else>
+        <div>
+          <ThreadsUtils mode="searching" :platform="$route.query.platform as string" />
+        </div>
+      </template>
     </div>
- </template>
+    <div class="flex w-full p-5 bg-slate-700 justify-center text-[#FFD95A] text-[16px]">
+      Botio X CPE
+    </div>
+  </div>
+</template>
 
 <script setup lang="ts">
 import ConversationThred from './ConversationsThread.vue';
