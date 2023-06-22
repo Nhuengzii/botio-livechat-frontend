21dbb62b9909db7e3799f5cf9a43b293bc5777e1
mahasachan <Mahasachan.sa@ku.th>
2023-06-23 00:50:11 +0700

    decorate this template

--- src/components/MessageContents/LineTemplatConfirm.vue
+++ src/components/MessageContents/LineTemplatConfirm.vue
@@ -1,10 +1,27 @@
-<template>
-  <div class="bg-gray-50 pt-2 rounded-lg">
-    <h1 class=""> {{ payload.text }}</h1>
-    <hr>
-    <div class="flex justify-evenly">
+<!-- <template>
+  <div class="bg-gray-200 pt-2 rounded-lg flex flex-col border-b items-center justify-center">
+    <div class="border-black border-b pb-2">
+      <h1>{{ payload.text }}</h1>
+    </div>
+    <hr class="bg-red-300">
+    <div class="flex">
       <div v-for="action in payload.actions" class="p-3 hover:bg-gray-100" :key="action.label">
         <a :href="action.uri" class="text-blue-500" target="_blank">{{ action.label }}</a>
+      </div>
+    </div>
+  </div>
+</template> -->
+
+<template>
+  <div class="flex items-center justify-end mr-4">
+    <div class="pt-2 rounded-lg flex flex-col basis-auto w-96 items-center self-center mb-4 mt-2">
+      <div class="flex justify-center border-gray-300 w-full border-b py-4 px-4 bg-gray-100 rounded-tl-xl rounded-tr-xl text-base break-all">
+        {{ payload.text }} 
+      </div>
+      <div class="flex bg-white rounded-bl-xl rounded-br-xl w-96">
+        <div v-for="action in payload.actions" class="flex justify-center py-6 w-48 rounded-bl-xl rounded-br-xl hover:bg-gray-50" :key="action.label">
+          <a :href="action.uri" class="text-blue-500" target="_blank">{{ action.label }}</a>
+        </div>
       </div>
     </div>
   </div>
