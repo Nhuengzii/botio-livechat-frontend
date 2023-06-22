603100d56a3c131a75bea5939ff5784e76ede26a
peebaKU <baramee.254450@gmail.com>
2023-06-22 16:29:06 +0700

    add space

--- src/components/ThreadsUtils.vue
+++ src/components/ThreadsUtils.vue
@@ -3,15 +3,15 @@
     <div class="bg-white rounded-t-3xl py-5 px-5">
       <div class="flex">
         <h1 class="text-slate-800">ข้อความทั้งหมด</h1>
-        <h1 class="ml-5 bg-[#F94A2999] rounded-xl"> 999+ </h1>
+        <h1 class="ml-5 bg-[#F94A2999] rounded-full px-2 font-semibold text-[#394867]"> 999+ </h1>
       </div>
-      <div class="relative w-full mr-4 ">
-          <div class="absolute inset-y-0  left-0 flex items-center pl-6 pointer-events-none"
+      <div class="relative w-full mr-4 mt-4">
+          <div class="absolute inset-y-0  left-0 flex items-center pl-4 pointer-events-none"
             :class="[querying ? '' : 'z-50']">
             <font-awesome-icon :icon="['fas', 'magnifying-glass']" />
           </div>
           <input type="text" :class="querying ? '' : ' rounded-full z-0'"
-            class="ml-2 bg-[#D9D9D9]  border border-gray-300 text-gray-900  outline-none   block w-full pl-10 p-2.5   dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            class="bg-[#D9D9D9]  border border-gray-300 text-gray-900  outline-none   block w-full pl-9 p-1  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             :placeholder="querying ? 'ค้นหา' : ' ค้นหาจากชื่อ หรือ แท็ก'" v-model="query"
             @click="() => { uiStore.conversationsThreadMode = 'searching' }">
       </div>
