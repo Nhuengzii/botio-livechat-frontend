6a0f2b13384adab28e507bfb1138f31706c81ec2
peebaKU <baramee.254450@gmail.com>
2023-06-22 14:05:27 +0700

    add box logoname botio x cpe

--- src/components/ThreadsUtils.vue
+++ src/components/ThreadsUtils.vue
@@ -6,33 +6,37 @@
         <h1 class="ml-5 bg-[#F94A2999] rounded-xl"> 999+ </h1>
       </div>
       <div class="relative w-full mr-4 ">
-        <div class="absolute inset-y-0  left-0 flex items-center pl-6 pointer-events-none"
-          :class="[querying ? '' : 'z-50']">
-          <font-awesome-icon :icon="['fas', 'magnifying-glass']" />
-        </div>
-        <input type="text" :class="querying ? '' : ' rounded-full z-0'"
-          class="ml-2 bg-[#D9D9D9]  border border-gray-300 text-gray-900  outline-none   block w-full pl-10 p-2.5   dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          :placeholder="querying ? 'ค้นหา' : ' ค้นหาจากชื่อ หรือ แท็ก'" v-model="query"
-          @click="() => { uiStore.conversationsThreadMode = 'searching' }">
+          <div class="absolute inset-y-0  left-0 flex items-center pl-6 pointer-events-none"
+            :class="[querying ? '' : 'z-50']">
+            <font-awesome-icon :icon="['fas', 'magnifying-glass']" />
+          </div>
+          <input type="text" :class="querying ? '' : ' rounded-full z-0'"
+            class="ml-2 bg-[#D9D9D9]  border border-gray-300 text-gray-900  outline-none   block w-full pl-10 p-2.5   dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            :placeholder="querying ? 'ค้นหา' : ' ค้นหาจากชื่อ หรือ แท็ก'" v-model="query"
+            @click="() => { uiStore.conversationsThreadMode = 'searching' }">
       </div>
     </div>
   </template>
   <template v-else-if="mode == 'searching'">
     <div>
-      <div class="flex">
-        <font-awesome-icon :icon="['fas', 'arrow-left']" size="xl" />
-        <input type="text" :class="querying ? '' : ' rounded-full z-0'"
-          class="ml-2 bg-[#D9D9D9]  border border-gray-300 text-gray-900  outline-none   block w-full pl-10 p-2.5   dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+      <div class="flex items-center ">
+        <div class="pl-8" @click="() => { uiStore.conversationsThreadMode = 'normal' } ">
+          <font-awesome-icon :icon="['fas', 'arrow-left']" size="xl" />
+        </div>
+        <div class="flex items-center w-full pr-4 pl-5 mr-8">
+          <input type="text" 
+          class="ml-2 bg-[#D9D9D9]  border border-gray-300 text-gray-900  outline-none   block w-full pl-10 p-1   dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           :placeholder="querying ? 'ค้นหา' : ' ค้นหาจากชื่อ หรือ แท็ก'" v-model="query">
-        <div @click="() => { uiStore.conversationsThreadMode = 'normal' }">
-          <font-awesome-icon :icon="['fas', 'xmark']" size="xl" />
+          <div class="  "
+            :class="[querying ? '' : 'z-50']">
+            <font-awesome-icon :icon="['fas', 'xmark']" size="xl" />
+          </div>
         </div>
       </div>
-      <div class="flex justify-between">
-        <button class="hover:bg-gray-300" :class="searchMode == 'by-message' ? 'bg-gray-300' : ''"
-          @click="() => { searchMode = 'by-message' }">แชทข้อความ</button>
-        <button class="hover:bg-gray-300" :class="searchMode == 'by-name' ? 'bg-gray-300' : ''"
-          @click="() => { searchMode = 'by-name' }">ชื่อสนทนา</button>
+      <div class="flex justify-between px-4 py-2">
+        <h1>ทั้งหมด</h1>
+        <h1>แชทข้อความ</h1>
+        <h1>ชื่อสนทนา</h1>
       </div>
       <div v-for="conversation in searchResult" :key="conversation.conversationID">
         <Thread :conversation="conversation" :show-platform="true" mode="searching" />
--- src/views/subs/LeftPanel.vue
+++ src/views/subs/LeftPanel.vue
@@ -1,25 +1,24 @@
 <template> 
-  <div class="bg-[#D9D9D9] min-w-[320px] max-w-[350px]  flex flex-col justify-between">
-    <div>
-      <template v-if="conversationsThreadMode == 'normal'">
-        <div class="px-2">
-          <ShopDisplay />
-        </div>        
-            <PlatformSelector class="px-2" />
-            <ThreadsUtils mode="normal" class="mx-2" />
-            <ConversationThred class="px-2 no-scrollbar" />
-      </template>
-      <template v-else>
-        <div>
-          <ThreadsUtils mode="searching" />
-        </div>
-      </template>
+    <div class="flex flex-col justify-between bg-[#D9D9D94c]  min-w-[320px] max-w-[350px]">
+      <div>
+        <template v-if="conversationsThreadMode == 'normal'">
+          <div class="px-2">
+            <ShopDisplay />
+          </div>        
+              <PlatformSelector class="px-2" />
+              <ThreadsUtils mode="normal" class="mx-2" />
+              <ConversationThred class="px-2 no-scrollbar" />
+        </template>
+        <template v-else>
+          <div>
+            <ThreadsUtils mode="searching" />
+          </div>
+        </template>
+      </div>
+      <div class="flex w-full p-5 bg-slate-700 justify-center text-[#FFD95A] text-[16px]">
+        Botio X CPE
+      </div>
     </div>
-    <div class="flex w-full p-5 bg-slate-700 justify-center text-[#FFD95A] text-[16px]">
-          Botio X CPE
-    </div>
-  </div>
-  
  </template>
 
 <script setup lang="ts">
