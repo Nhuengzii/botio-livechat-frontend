d025e513cf7cf32cf664c90d623a6008238105f0
mahasachan <Mahasachan.sa@ku.th>
2023-06-23 02:02:12 +0700

    add carousel and change ui

--- src/components/MessageContents/FacebookTemplateGeneric.vue
+++ src/components/MessageContents/FacebookTemplateGeneric.vue
@@ -1,7 +1,37 @@
 <template>
-  <div class="flex">
-    <div v-for="element in template.elements">
-      <div class="rounded-t-3xl  h-64 overflow-hidden">
+  <div class="flex items-center justify-end mr-4">
+    <Carousel :items-to-show="template.elements.length" :wrap-around="false" class="mb-4 rounded-xl"
+      :class="{ 'w-80': template.elements.length === 1 }">
+      <Slide v-for="element, index in template.elements" :key="index">
+        <div class="mx-2 border-2 rounded-xl">
+          <div class="rounded-t-xl h-64 overflow-hidden">
+            <img :src="element.image_url" class="object-cover rounded-t-xl" alt="">
+          </div>
+          <div class="rounded-b-xl bg-white pb-2 pt-1">
+            <div class="pb-2 border-b-2">
+              <h1 class="font-bold break-all">{{ element.title }}</h1>
+              <h2 class="break-all">{{ element.subtitle }}</h2>
+            </div>
+            <div class="flex justify-center bg-white border-b-2 py-4 hover:bg-gray-100" v-for="button, index in element.buttons"
+              :key="index">
+              <a :href="button.url" target="_blank" class="text-blue-500">{{ button.title }}</a>
+            </div>
+          </div>
+        </div>
+
+      </Slide>
+      <template #addons>
+        <Navigation />
+      </template>
+    </Carousel>
+  </div>
+  <p class="self-end pl-2 pb-1 text-sm text-[#B2B2B2]">{{ formatTimestamp(message.timestamp) }}</p>
+
+
+  <!-- <Carousel :items-to-show="2.5" :wrap-around="false" class="mb-4">
+    <Slide v-for="element in template.elements">
+      <div class="carousel__item mx-2">
+        <div class="rounded-t-3xl  h-64 overflow-hidden">
         <img :src="element.image_url" class="object-contain rounded-t-3xl" alt="">
       </div>
       <div class="rounded-b-3xl bg-gray-200 pb-2 pt-1">
@@ -14,13 +44,18 @@
           <a :href="button.url" target="_blank" class="">{{ button.title }}</a>
         </div>
       </div>
-    </div>
-  </div>
-  <p class="self-end pl-2 pb-1 text-sm text-[#B2B2B2]">{{ formatTimestamp(message.timestamp) }}</p>
+      </div>
+    </Slide>
+    <template #addons>
+      <Navigation/>
+    </template>
+  </Carousel> -->
 </template>
 
 <script setup lang="ts">
 import type { Message } from '@/types/message'
+import 'vue3-carousel/dist/carousel.css'
+import { Carousel, Navigation, Slide, } from 'vue3-carousel'
 const { message } = defineProps<{ message: Message }>()
 type Template = {
   elements: {
