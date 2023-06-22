5b62d59821a0bb340cb715a06f8b6535d7d3ac58
mahasachan <Mahasachan.sa@ku.th>
2023-06-23 00:14:44 +0700

    resize template

--- src/components/MessageContents/LineTemplatButtons.vue
+++ src/components/MessageContents/LineTemplatButtons.vue
@@ -1,15 +1,15 @@
 <template>
-  <div>
-    <div class="rounded-t-lg  h-64 overflow-hidden">
+  <div class="mb-4 mt-2 w-80">
+    <div class="rounded-t-lg h-56 overflow-hidden object-cover">
       <img :src="payload.thumbnailImageUrl" class="object-contain rounded-t-3xl" alt="">
     </div>
-    <div class="rounded-b-lg bg-gray-50 pb-2 pt-1">
+    <div class="rounded-b-lg bg-gray-100 pb-2 pt-1">
       <div class="ml-3 mb-3">
         <h1 class="font-bold">{{ payload.title }}</h1>
         <p>{{ payload.text }}</p>
       </div>
       <hr>
-      <div class="flex justify-center my-2 py-1 hover:bg-gray-100" v-for="button in payload.actions">
+      <div class="flex justify-center my-1 py-3 hover:bg-gray-200" v-for="button in payload.actions">
         <a :href="button.uri" target="_blank" class="text-blue-500">{{ button.label }}</a>
       </div>
     </div>
