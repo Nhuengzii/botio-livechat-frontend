8d169e2f3aaaa187566c9f6e910f8c97b5ec9fc5
mahasachan <Mahasachan.sa@ku.th>
2023-06-24 01:38:46 +0700

    add animation spinner

--- src/views/subs/Chats.vue
+++ src/views/subs/Chats.vue
@@ -80,15 +80,21 @@
 
       <main class="flex-[12] overflow-x-hidden no-scrollbar h-full bg-white mb-4  mx-3" id="containMessage"
         ref="conversationRef">
-        <InfiniteLoading @infinite="loadmore" :firstload="false" :top="true"
-          :identifier="currentChat?.conversation.conversationID">
-          <template #spinner>
-            <span>loading...</span>
-          </template>
-          <template #complete>
-            <span>No more data found!</span>
-          </template>
-        </InfiniteLoading>
+        <template v-if="currentChat !== null">
+          <InfiniteLoading @infinite="loadmore" :firstload="false" :top="true"
+            :identifier="currentChat?.conversation.conversationID">
+            <template #spinner>
+              <div class="flex justify-center pt-5 pb-3">
+                <span class="loader"></span>
+              </div>
+  
+  
+            </template>
+            <template #complete>
+              <span>No more data found!</span>
+            </template>
+          </InfiniteLoading>
+        </template>
         <div class="grid grid-cols-12 gap-y-0.5">
           <template v-for="(message, index, timestamp) in currentChat?.messages" :key="message.messageID">
             <template v-if="isNewDay(index)">
@@ -366,5 +372,72 @@ button {
   color: #000000;
   transition: 0.5s;
 }
-</style>
+
+.loader {
+  color: blue;
+  font-size: 32px;
+  text-indent: -9999em;
+  overflow: hidden;
+  width: 1em;
+  height: 1em;
+  border-radius: 50%;
+  position: relative;
+  transform: translateZ(0);
+  animation: mltShdSpin 1.7s infinite ease, round 1.7s infinite ease;
+}
+
+@keyframes mltShdSpin {
+  0% {
+    box-shadow: 0 -0.83em 0 -0.4em,
+      0 -0.83em 0 -0.42em, 0 -0.83em 0 -0.44em,
+      0 -0.83em 0 -0.46em, 0 -0.83em 0 -0.477em;
+      /* color: #E3FDFD; */
+  }
+
+  5%,
+  95% {
+    box-shadow: 0 -0.83em 0 -0.4em,
+      0 -0.83em 0 -0.42em, 0 -0.83em 0 -0.44em,
+      0 -0.83em 0 -0.46em, 0 -0.83em 0 -0.477em;
+      /* color: #CBF1F5; */
+  }
+
+  10%,
+  59% {
+    box-shadow: 0 -0.83em 0 -0.4em,
+      -0.087em -0.825em 0 -0.42em, -0.173em -0.812em 0 -0.44em,
+      -0.256em -0.789em 0 -0.46em, -0.297em -0.775em 0 -0.477em;
+      /* color: #A6E3E9; */
+  }
+
+  20% {
+    box-shadow: 0 -0.83em 0 -0.4em, -0.338em -0.758em 0 -0.42em,
+      -0.555em -0.617em 0 -0.44em, -0.671em -0.488em 0 -0.46em,
+      -0.749em -0.34em 0 -0.477em;
+      /* color: #71C9CE; */
+  }
+
+  38% {
+    box-shadow: 0 -0.83em 0 -0.4em, -0.377em -0.74em 0 -0.42em,
+      -0.645em -0.522em 0 -0.44em, -0.775em -0.297em 0 -0.46em,
+      -0.82em -0.09em 0 -0.477em;
+      /* color: #71C9CE; */
+  }
+
+  100% {
+    box-shadow: 0 -0.83em 0 -0.4em, 0 -0.83em 0 -0.42em,
+      0 -0.83em 0 -0.44em, 0 -0.83em 0 -0.46em, 0 -0.83em 0 -0.477em;
+      /* color: #71C9CE; */
+  }
+}
+
+@keyframes round {
+  0% {
+    transform: rotate(0deg)
+  }
+
+  100% {
+    transform: rotate(360deg)
+  }
+}</style>
 
