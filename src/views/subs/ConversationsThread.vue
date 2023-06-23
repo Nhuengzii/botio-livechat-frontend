7c0bcbf9cbb5933f4ee49f520aa5b360dd40a17f
Nhuengzii <32nhueng@gmail.com>
2023-06-23 13:41:13 +0700

    delete burst conversation function

--- src/views/subs/ConversationsThread.vue
+++ src/views/subs/ConversationsThread.vue
@@ -1,13 +1,13 @@
 <template>
-  <div >
-  <ThreadSkeleton :num-skeletons="6" v-if="isLoading" />
-  <div v-bind="containerProps" v-show="!isLoading" >
-    <div v-bind="wrapperProps">
-      <div v-for="({ data }, index) in list" :key="data.conversationID">
-        <Thread :conversation="data" :show-platform="$route.query.platform == 'all'" :mode="conversationsThreadMode" />
+  <div>
+    <ThreadSkeleton :num-skeletons="6" v-if="isLoading" />
+    <div v-bind="containerProps" v-show="!isLoading">
+      <div v-bind="wrapperProps">
+        <div v-for="({ data }, index) in list" :key="data.conversationID">
+          <Thread :conversation="data" :show-platform="$route.query.platform == 'all'" :mode="conversationsThreadMode" />
+        </div>
       </div>
     </div>
-  </div>
   </div>
 </template>
 
@@ -49,21 +49,6 @@ onMounted(async () => {
   await livechatStore.fetchConversations(route.query.platform as string)
   isLoading.value = false;
 });
-
-function burstConversation() {
-  const currentPlatform = route.query.platform as string;
-  const conversation = conversations.value(currentPlatform)[0];
-  const conversationMap = livechatStore.conversationRaw.get(currentPlatform)!
-  for (let i = 0; i < 100; i++) {
-    const newConversation = {
-      ...conversation
-    }
-    const randomAvatarUrl = `https://picsum.photos/seed/${Math.random().toString(36).substring(7)}/200/300`
-    newConversation.participants[0].profilePic.src = randomAvatarUrl;
-    newConversation.conversationID = Math.random().toString(36).substring(7);
-    conversationMap.set(newConversation.conversationID, newConversation)
-  }
-}
 
 
 </script>
