a5c089efadfd299f3b2eb61bd10e0b9fecda6380
mahasachan <mahasachan.sa@ku.th>
2023-06-26 22:33:57 +0700

    init type template

---
+++ src/components/ModalTemplateChats/TemplateType/TemplateButton.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="flex mx-2 my-4 px-4">
+        <div class="bg-white  border-2 rounded-lg">
+            <div class="flex items-center justify-center w-52 h-32 bg-blue-700 rounded-t-lg">
+                <p class="text-white">image</p>
+                <img src="" alt="">
+            </div>
+            <div class="border-b-2 flex flex-col items-start">
+                <h1 class="break-all pl-3 pt-2 font-semibold">หัวข้อ</h1>
+                <p class="break-all pl-3 pb-4">ข้อความ . . .</p>
+            </div>
+            <div class="flex flex-col">
+                <button class="bg-gray-100 hover:bg-gray-100 border-b-2 py-2">ปุ่มกดที่1</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+</script>
+
+<style scoped></style>
---
+++ src/components/ModalTemplateChats/TemplateType/TemplateTextImage.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="flex mx-2 my-4 px-4">
+        <div class="bg-white  border-2 rounded-lg">
+            <div class="flex items-center justify-center w-52 h-44 bg-blue-700 rounded-t-lg">
+                <p class="text-white">image</p>
+                <img src="" alt="">
+            </div>
+            <div class="border-b-2 flex flex-col items-start">
+                <h1 class="break-all pl-3 pt-2 font-semibold">หัวข้อ</h1>
+                <p class="break-all pl-3 pb-4">ข้อความ . . .</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped></style>
