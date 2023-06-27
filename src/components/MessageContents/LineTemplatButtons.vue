33fe0e88d79ce946962188a58ee2029a185f373f
mahasachan <mahasachan.sa@ku.th>
2023-06-27 21:27:52 +0700

    decrease padding y in button

--- src/components/MessageContents/LineTemplatButtons.vue
+++ src/components/MessageContents/LineTemplatButtons.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="mb-4 mt-2 w-80 border-2 rounded-2xl">
+  <div class="mb-4 mt-2  w-80 border-2 rounded-2xl">
     <div class="rounded-t-2xl h-56 overflow-hidden object-cover">
       <img :src="payload.thumbnailImageUrl" class="object-contain" alt="">
     </div>
@@ -9,7 +9,7 @@
         <p class="break-all px-4">{{ payload.text }}</p>
       </div>
       <hr>
-      <div class="flex justify-center mb-2 py-4 hover:bg-gray-50" v-for="button in payload.actions">
+      <div class="flex justify-center mb-2 py-2 hover:bg-gray-50" v-for="button in payload.actions">
         <a :href="button.uri" target="_blank" class="text-blue-500">{{ button.label }}</a>
       </div>
     </div>
