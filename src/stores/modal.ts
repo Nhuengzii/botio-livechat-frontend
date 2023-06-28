c59a2db786ff198c403c90a4bb258067592f4514
mahasachan <Mahasachan.sa@ku.th>
2023-06-29 01:18:02 +0700

    add imagePreview, textUserInput, titleUserInput

--- src/stores/modal.ts
+++ src/stores/modal.ts
@@ -3,11 +3,34 @@ import { defineStore } from 'pinia'
 
 export const useModalStore = defineStore('modal', {
     state: () => ({
-        selectedTemplate:""
+        selectedTemplate: "",
+        imagePreview: "",
+        textUserInput: "",
+        titleUserInput: ""
     }),
     actions: {
-        selectTemplate(template:string){
+        selectTemplate(template: string) {
             this.selectedTemplate = template
+        },
+        handleFileSelect(event: Event) {
+            const file = (event.target as HTMLInputElement).files?.[0];
+            //console.log(file)
+            if (file) {
+
+                // Read the selected file and generate a preview URL
+                const reader = new FileReader();
+                reader.onload = () => {
+                    this.imagePreview = reader.result as string;
+                };
+                reader.readAsDataURL(file);
+                console.log(this.imagePreview)
+            }
+        },
+        reset() {
+            this.imagePreview = ""
+            this.imagePreview = ""
+            this.textUserInput = ""
+            this.titleUserInput = ""
         }
     }
 })
