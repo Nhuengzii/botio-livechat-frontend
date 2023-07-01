a3020b244c33877f9295a67b2b08b6fb9b81941a
mahasachan <Mahasachan.sa@ku.th>
2023-07-01 21:38:42 +0700

    change size&spaceing

--- src/components/ModalTemplateChats/TemplateType/TemplateTextImage.vue
+++ src/components/ModalTemplateChats/TemplateType/TemplateTextImage.vue
@@ -1,25 +1,29 @@
 <template>
     <div class="flex mx-2 my-4 px-4">
         <div class="bg-white  border-2 rounded-lg">
-            <div class="flex items-center justify-center w-64 h-44 bg-blue-700 rounded-t-lg">
+
+            <div class="flex items-center justify-center w-60 h-44 bg-blue-700 rounded-t-lg">
                 <img :src="modalStore.imagePreview" alt="" class="object-cover h-full w-full overflow-hidden" v-if="modalStore.imagePreview">
                 <p class="text-white" v-else>image</p>
                 
             </div>
-            <div class="border-b-2 flex flex-col items-start">
-                <h1 class="break-all px-3 py-2 max-h-16 w-64 font-semibold text-ellipsis" v-if=" modalStore.titleUserInput">
+
+            <div class="border-b-2 flex flex-col items-start w-60">
+                <h1 class="break-all px-3 py-2 max-h-16  font-semibold text-ellipsis" v-if=" modalStore.titleUserInput">
                     {{  modalStore.titleUserInput }}
                 </h1>
                 <h1 class="break-all px-3 py-2 font-semibold" v-else>
                     หัวข้อ
                 </h1>
-                <p class="break-all px-3 pb-4 max-h-48 w-64 text-ellipsis overflow-hidden" v-if="modalStore.textUserInput">
+                <p class="break-all px-3 pb-4 min-h-32 max-h-48 text-ellipsis overflow-hidden" v-if="modalStore.textUserInput">
                     {{ modalStore.textUserInput }}
                 </p>
                 <p class="break-all pl-3 pb-4" v-else>
                     ข้อความ . . .
                 </p>
             </div>
+
+
         </div>
     </div>
 </template>
