52d637705004057bd497a0a6bcbe8ebdb0ae8844
Nhuengzii <32nhueng@gmail.com>
2023-07-02 19:09:49 +0700

    load page information when start

--- src/App.vue
+++ src/App.vue
@@ -9,11 +9,9 @@ import type { PageInformation } from '@/types/pageInformation'
 onMounted(async () => {
   const availablePages = await livechatStore.getShopInformation()
   for (const page of availablePages.available_pages) {
-    uiStore.availablesPlatforms.set(page.platform_name, {
-      unreadConversations: 0,
-      allConversations: 0,
-    })
     livechatStore.pageIDMap.set(page.platform_name, page.page_id)
+    const pageInformation = await livechatStore.getPageInformation(page.platform_name);
+    uiStore.addAvailablePlatform(page.platform_name, pageInformation)
   }
   console.log(JSON.stringify(availablePages))
 })
