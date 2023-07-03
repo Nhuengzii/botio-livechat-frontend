7a77efa2cba9d36dd3858d4b4b08e9f2b3d1a1c8
Nhuengzii <32nhueng@gmail.com>
2023-07-03 23:22:23 +0700

    findShopID and then setup BotioLivecgat then redirect to `/livechat`

--- src/views/Root.vue
+++ src/views/Root.vue
@@ -5,14 +5,19 @@
 </template>
 
 <script setup lang="ts">
+import { useLivechatStore } from '@/stores/livechat';
 import { onMounted } from 'vue';
-
+import { useRouter } from 'vue-router';
+const livechatStore = useLivechatStore()
+const router = useRouter()
 function findShopID() {
   // custom logic
   return "1"
 }
 onMounted(() => {
   const shopID = findShopID()
+  livechatStore.setupBotioLivechat(shopID)
+  router.push({ path: "/livechat", query: { platform: "all" } })
 })
 </script>
 
