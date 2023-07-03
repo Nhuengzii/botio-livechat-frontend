82c96b5a3f591d90739a0ad6c0146384da0fd57c
Nhuengzii <32nhueng@gmail.com>
2023-07-03 23:33:11 +0700

    move livechat information setup logic to livechat vue

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,5 @@
 <script setup lang="ts">
-import { onMounted } from 'vue';
 import { RouterView } from 'vue-router';
-import { useLivechatStore } from './stores/livechat';
-import { useUIStore } from './stores/UI';
-const livechatStore = useLivechatStore()
-const uiStore = useUIStore();
-onMounted(async () => {
-  const availablePages = await livechatStore.getShopInformation()
-  for (const page of availablePages.available_pages) {
-    livechatStore.pageIDMap.set(page.platform_name, page.page_id)
-  }
-  const allPageInformation = await livechatStore.getAllPageInformation()
-  allPageInformation.statuses.forEach((status => {
-    uiStore.addAvailablePlatform(status.platform, { unreadConversations: status.unreadConversations, allConversations: status.allConversations })
-  }))
-})
 </script>
 
 <template >
--- src/views/Livechat.vue
+++ src/views/Livechat.vue
@@ -13,7 +13,21 @@
 <script setup lang="ts">
 import LeftPanel from './subs/LeftPanel.vue';
 import Chats from './subs/Chats.vue';
-
+import { onMounted } from 'vue';
+import { useLivechatStore } from '@/stores/livechat';
+import { useUIStore } from '@/stores/UI';
+const livechatStore = useLivechatStore()
+const uiStore = useUIStore();
+onMounted(async () => {
+    const availablePages = await livechatStore.getShopInformation()
+    for (const page of availablePages.available_pages) {
+        livechatStore.pageIDMap.set(page.platform_name, page.page_id)
+    }
+    const allPageInformation = await livechatStore.getAllPageInformation()
+    allPageInformation.statuses.forEach((status => {
+        uiStore.addAvailablePlatform(status.platform, { unreadConversations: status.unreadConversations, allConversations: status.allConversations })
+    }))
+})
 </script>
 
 <style scoped></style>
