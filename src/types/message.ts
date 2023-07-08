0809d4ac6c46760ee9d44b279a01d26482d17049
Nhuengzii <32nhueng@gmail.com>
2023-07-08 13:20:13 +0700

    add more Template Type in message.ts

--- src/types/message.ts
+++ src/types/message.ts
@@ -30,4 +30,80 @@ type RepliedMessage = {
   messageID: string
 }
 
-export type { Message, Attachment, Source, Payload }
+type FacebookTemplateGeneric = {
+  fb_template_generic: {
+    title: string,
+    message: string,
+    picture: string,
+    buttons: {
+      url: string,
+      title: string,
+    }[]
+    default_action: {
+      url: string
+    }
+  }[]
+}
+
+type LineTemplateButtons = {
+  line_template_buttons: {
+    altText: string,
+    thumbnailImageUrl: string,
+    title: string,
+    text: string,
+    defaultAction: {
+      label: string,
+      uri: string
+    },
+    actions: {
+      label: string,
+      uri: string,
+    }[]
+  }
+}
+
+type LineTemplateConfirm = {
+  line_template_confirm: {
+    altText: string,
+    text: string,
+    actions: {
+      label: string,
+      uri: string
+    }[]
+  }
+}
+
+type LineTemplateImageCarousel = {
+  line_template_image_carousel: {
+    altText: string,
+    columns: {
+      imageUrl: string,
+      action: {
+        label: string,
+        uri: string
+      }
+    }[]
+  }
+}
+
+type LineTemplateCarosel = {
+  line_template_carousel: {
+    altText: string,
+    columns: {
+      thumbnailImageUrl: string,
+      title: string,
+      text: string,
+      defaultAction: {
+        label: string,
+        uri: string
+      },
+      actions: {
+        label: string,
+        uri: string
+      }[]
+    }[]
+  }
+}
+
+
+export type { Message, Attachment, Source, Payload, FacebookTemplateGeneric, LineTemplateButtons, LineTemplateConfirm, LineTemplateCarosel, LineTemplateImageCarousel }
